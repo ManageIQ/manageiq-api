f944ae059b0aef420d4e3c9c6f9c9c45dd7dcc08
Joe Rafaniello <jrafanie@redhat.com>
2017-07-28 15:25:46 -0400

    Look for the config in the engine.root, not rails.root

--- lib/api/api_config.rb
+++ lib/api/api_config.rb
@@ -2,7 +2,7 @@ require "config"
 
 module Api
   ApiConfig = ::Config::Options.new.tap do |o|
-    o.add_source!(Rails.root.join("config/api.yml").to_s)
+    o.add_source!(ManageIQ::Api::Engine.root.join("config/api.yml").to_s)
     o.load!
   end
 end
