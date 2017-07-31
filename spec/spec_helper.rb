60def08cebb4f36cda9d750b2baa876e14f19da7
Joe Rafaniello <jrafanie@redhat.com>
2017-07-31 14:24:51 -0400

    Move api test config from the main repo to here

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,3 +7,14 @@ require 'manageiq-api'
 
 Dir[ManageIQ::Api::Engine.root.join("spec/support/**/*.rb")].each { |f| require f }
 Dir[Rails.root.join("spec/shared/**/*.rb")].each { |f| require f }
+
+RSpec.configure do |config|
+  config.include Spec::Support::ApiHelper, :rest_api => true
+  config.include Spec::Support::AuthRequestHelper, :type => :request
+  config.define_derived_metadata(:file_path => /spec\/requests\/api/) do |metadata|
+    metadata[:aggregate_failures] = true
+    metadata[:rest_api] = true
+  end
+
+  config.before(:each, :rest_api => true) { init_api_spec_env }
+end
