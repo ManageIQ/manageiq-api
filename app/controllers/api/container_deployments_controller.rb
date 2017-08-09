24232ca42982d34bf7126abe3ead59389abe5825
Tim Wade <hello@timjwade.com>
2017-08-09 08:47:01 -0700

    Extract private method authentication_requested?

--- app/controllers/api/container_deployments_controller.rb
+++ app/controllers/api/container_deployments_controller.rb
@@ -9,11 +9,19 @@ module Api
       # TODO: this service is rendering resources which (a) require
       # authentication (problematic for CORS compatibility), and (b)
       # are not being properly filtered by RBAC
-      if [HttpHeaders::MIQ_TOKEN, HttpHeaders::AUTH_TOKEN, "HTTP_AUTHORIZATION"].any? { |header| request.headers.include?(header) }
+      if authentication_requested?
         require_api_user_or_token
         render_options(:container_deployments, ContainerDeploymentService.new.all_data)
       else
         super
+      end
+    end
+
+    private
+
+    def authentication_requested?
+      [HttpHeaders::MIQ_TOKEN, HttpHeaders::AUTH_TOKEN, "HTTP_AUTHORIZATION"].any? do |header|
+        request.headers.include?(header)
       end
     end
   end
