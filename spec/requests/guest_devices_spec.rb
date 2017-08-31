25b7d1dc0896360f39bffc4aba794913ee18255d
skovic <skovic@lenovo.com>
2017-08-31 14:51:46 -0400

    Fix test case wording and move test file into correct directory

--- spec/requests/guest_devices_spec.rb
+++ spec/requests/guest_devices_spec.rb
@@ -1,7 +1,7 @@
 RSpec.describe "guest devices API" do
   describe "display guest device details" do
-    context "with a valid role" do
-      it "shows its properties" do
+    context "with the user authorized" do
+      it "responds with device properties" do
         device = FactoryGirl.create(:guest_device,
                                     :device_name => "Broadcom 2-port 1GbE NIC Card",
                                     :device_type => "ethernet",
@@ -17,8 +17,8 @@ RSpec.describe "guest devices API" do
       end
     end
 
-    context "with an invalid role" do
-      it "fails to show its properties" do
+    context "with the user unauthorized" do
+      it "responds with a forbidden status" do
         device = FactoryGirl.create(:guest_device)
 
         api_basic_authorize
