9b8772477821f9419c283388b2c972eca1144799
Aparna Karve <akarve@redhat.com>
2017-08-31 17:19:54 -0700

    OPTIONS for Generic Obj Definitions
    Return allowed associated classes and types

--- app/controllers/api/generic_object_definitions_controller.rb
+++ app/controllers/api/generic_object_definitions_controller.rb
@@ -25,6 +25,16 @@ module Api
       raise BadRequestError, "Failed to delete generic object definition - #{err}"
     end
 
+    def options
+      render_options(:generic_object_definitions, build_generic_object_definition_options)
+    end
+
+    def build_generic_object_definition_options
+      {
+        :allowed_association_types => allowed_association_types,
+        :allowed_types             => allowed_types
+      }
+    end
     private
 
     def resource_search(id, type, klass)
@@ -34,6 +44,18 @@ module Api
         go_def = klass.find_by!(:name => id)
         filter_resource(go_def, type, klass)
       end
+    end
+
+    def allowed_association_types
+      GenericObjectDefinition::ALLOWED_ASSOCIATION_TYPES.collect do |association_type|
+        [Dictionary.gettext(association_type, :type => :model, :notfound => :titleize, :plural => false), association_type]
+      end.sort
+    end
+
+    def allowed_types
+      GenericObjectDefinition::TYPE_MAP.keys.collect do |type|
+        [Dictionary.gettext(type.to_s, :type => :data_type, :notfound => :titleize, :plural => false), type]
+      end.sort
     end
   end
 end
