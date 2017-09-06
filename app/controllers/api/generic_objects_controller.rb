5d5fbf359a8363e36292393f1849f4e82cab1470
Jillian Tullo <jtullo@redhat.com>
2017-09-06 14:25:15 -0400

    create generic objects

---
+++ app/controllers/api/generic_objects_controller.rb
@@ -0,0 +1,31 @@
+module Api
+  class GenericObjectsController < BaseController
+    EXCEPTION_ATTRS = %w(generic_object_definition associations).freeze
+
+    def create_resource(_type, _id, data)
+      object_def = retrieve_generic_object_definition(data)
+      generic_object = object_def.create_object(data.except(*EXCEPTION_ATTRS))
+      add_associations(generic_object, data)
+    rescue => err
+      raise BadRequestError, "Failed to create new generic object - #{err}"
+    end
+
+    private
+
+    def retrieve_generic_object_definition(data)
+      definition_id = parse_id(data['generic_object_definition'], :generic_object_definitions)
+      resource_search(definition_id, :generic_object_definitions, collection_class(:generic_object_definitions))
+    end
+
+    def add_associations(generic_object, data)
+      data['associations'].each do |association, resource_refs|
+        resources = resource_refs.collect do |ref|
+          collection, id = parse_href(ref['href'])
+          resource_search(id, collection, collection_class(collection))
+        end
+        generic_object.add_to_property_association(association, resources)
+      end
+      generic_object
+    end
+  end
+end
---
+++ spec/requests/generic_objects_spec.rb
@@ -0,0 +1,58 @@
+RSpec.describe 'GenericObjects API' do
+  let(:object_definition) { FactoryGirl.create(:generic_object_definition, :name => 'object def') }
+  let(:vm) { FactoryGirl.create(:vm_amazon) }
+  let(:vm2) { FactoryGirl.create(:vm_amazon) }
+  let(:service) { FactoryGirl.create(:service) }
+
+  before do
+    object_definition.add_property_attribute('widget', 'string')
+    object_definition.add_property_attribute('is_something', 'boolean')
+
+    object_definition.add_property_association('services', 'Service')
+    object_definition.add_property_association('vms', 'Vm')
+  end
+
+  describe 'POST /api/generic_objects' do
+    it 'does not allow creation of a generic object without appropriate role' do
+      api_basic_authorize
+
+      run_post(generic_objects_url)
+
+      expect(response).to have_http_status(:forbidden)
+    end
+
+    it 'can create a new generic object' do
+      api_basic_authorize collection_action_identifier(:generic_objects, :create)
+
+      generic_object = {
+        'generic_object_definition' => { 'href' => generic_object_definitions_url(object_definition.compressed_id) },
+        'name'                      => 'go_name1',
+        'uid'                       => 'optional_uid',
+        'property_attributes'       => {
+          'widget'       => 'widget value',
+          'is_something' => false
+        },
+        'associations'              => {
+          'vms'      => [
+            { 'href' => vms_url(vm.compressed_id) },
+            { 'href' => vms_url(vm2.compressed_id) }
+          ],
+          'services' => [
+            { 'href' => services_url(service.compressed_id) }
+          ]
+        }
+      }
+      run_post(generic_objects_url, generic_object)
+
+      expected = {
+        'results' => [
+          a_hash_including('name'                         => 'go_name1',
+                           'uid'                          => 'optional_uid',
+                           'generic_object_definition_id' => object_definition.compressed_id)
+        ]
+      }
+      expect(response).to have_http_status(:ok)
+      expect(response.parsed_body).to include(expected)
+    end
+  end
+end
