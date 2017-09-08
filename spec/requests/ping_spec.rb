01c82651ca23b9751d01e9dc3a8fa400ae6a80f4
Brandon Dunne <bdunne@redhat.com>
2017-09-08 11:58:58 -0400

    Add a basic ping endpoint
    Allows us to know whether the API server is ready to serve requests

---
+++ spec/requests/ping_spec.rb
@@ -0,0 +1,8 @@
+describe "PingController" do
+  it "get" do
+    run_get(api_ping_url)
+
+    expect(response).to have_http_status(:success)
+    expect(response.parsed_body).to eq("pong")
+  end
+end
