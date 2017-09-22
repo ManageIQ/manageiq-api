674f9dd72f7e033cc201a49203692ab1ab03bbde
Tim Wade <hello@timjwade.com>
2017-09-22 14:15:22 -0700

    Extract HrefParser service

--- app/controllers/api/base_controller/parser.rb
+++ app/controllers/api/base_controller/parser.rb
@@ -52,24 +52,7 @@ module Api
       #   collection/c_id/<subcollection>/<s_id>
       #
       def parse_href(href)
-        if href
-          path = href.match(/^http/) ? URI.parse(href).path.sub!(/\/*$/, '') : href.dup
-          path.prepend("/")     unless path.start_with?("/")
-          path.prepend("/api")  unless path.match("/api")
-          path.sub!(/\/*$/, '')
-          return href_collection_id(path)
-        end
-        [nil, nil]
-      end
-
-      def href_collection_id(path)
-        path_array = path.split('/')
-        cidx = path_array[2] && path_array[2].match(Api::VERSION_REGEX) ? 3 : 2
-
-        collection, c_id    = path_array[cidx..cidx + 1]
-        subcollection, s_id = path_array[cidx + 2..cidx + 3]
-
-        subcollection ? [subcollection.to_sym, ApplicationRecord.uncompress_id(s_id)] : [collection.to_sym, ApplicationRecord.uncompress_id(c_id)]
+        HrefParser.parse(href)
       end
 
       def parse_id(resource, collection)
---
+++ lib/services/api/href_parser.rb
@@ -0,0 +1,25 @@
+module Api
+  class HrefParser
+    def self.parse(href)
+      if href
+        path = href.match(/^http/) ? URI.parse(href).path.sub!(/\/*$/, '') : href.dup
+        path.prepend("/")     unless path.start_with?("/")
+        path.prepend("/api")  unless path.match("/api")
+        path.sub!(/\/*$/, '')
+        return href_collection_id(path)
+      end
+      [nil, nil]
+    end
+
+    def self.href_collection_id(path)
+      path_array = path.split('/')
+      cidx = path_array[2] && path_array[2].match(Api::VERSION_REGEX) ? 3 : 2
+
+      collection, c_id    = path_array[cidx..cidx + 1]
+      subcollection, s_id = path_array[cidx + 2..cidx + 3]
+
+      subcollection ? [subcollection.to_sym, ApplicationRecord.uncompress_id(s_id)] : [collection.to_sym, ApplicationRecord.uncompress_id(c_id)]
+    end
+    private_class_method :href_collection_id
+  end
+end
