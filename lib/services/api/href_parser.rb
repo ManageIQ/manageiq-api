9f798d3ed6eaef782f6f8e36eaa90671ff9ab629
Tim Wade <hello@timjwade.com>
2017-09-22 15:15:14 -0700

    Extract #path_parts

--- lib/services/api/href_parser.rb
+++ lib/services/api/href_parser.rb
@@ -38,13 +38,16 @@ module Api
     end
 
     def href_collection_id
-      path_array = path.split('/')
-      cidx = path_array[2] && path_array[2].match(Api::VERSION_REGEX) ? 3 : 2
+      cidx = path_parts[2] && path_parts[2].match(Api::VERSION_REGEX) ? 3 : 2
 
-      collection, c_id    = path_array[cidx..cidx + 1]
-      subcollection, s_id = path_array[cidx + 2..cidx + 3]
+      collection, c_id    = path_parts[cidx..cidx + 1]
+      subcollection, s_id = path_parts[cidx + 2..cidx + 3]
 
       subcollection ? [subcollection.to_sym, ApplicationRecord.uncompress_id(s_id)] : [collection.to_sym, ApplicationRecord.uncompress_id(c_id)]
+    end
+
+    def path_parts
+      path.split("/")
     end
   end
 end
