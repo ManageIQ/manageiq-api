b04eccf52034e6e789b2d28934dd1adab535899c
mkanoor <mkanoor@redhat.com>
2017-09-22 15:23:39 -0400

    Removed index use the one from base controller

--- app/controllers/api/automate_workspaces_controller.rb
+++ app/controllers/api/automate_workspaces_controller.rb
@@ -1,18 +1,5 @@
 module Api
   class AutomateWorkspacesController < BaseController
-    def index
-      klass = collection_class(@req.subject)
-      res, subquery_count = collection_search(@req.subcollection?, @req.subject, klass)
-      opts = {
-        :name             => @req.subject,
-        :is_subcollection => @req.subcollection?,
-        :expand_actions   => true,
-        :expand_resources => @req.expand?(:resources),
-        :counts           => Api::QueryCounts.new(klass.count, res.count, subquery_count)
-      }
-      render_collection(:automate_workspaces, res, opts)
-    end
-
     def show
       obj = AutomateWorkspace.find_by(:guid => @req.c_id)
       if obj.nil?
