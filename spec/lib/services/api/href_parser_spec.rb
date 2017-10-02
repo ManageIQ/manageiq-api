9d4750407dfc0113bfec264362052db931124ff5
Tim Wade <hello@timjwade.com>
2017-10-02 14:23:12 -0700

    Update HrefParser tests to test instance attributes

--- spec/lib/services/api/href_parser_spec.rb
+++ spec/lib/services/api/href_parser_spec.rb
@@ -1,66 +1,66 @@
 RSpec.describe Api::HrefParser do
-  describe ".parse" do
+  describe ".new" do
     context "with a full url for the href" do
       let(:href) { "http://localhost:3000/api/collection/123" }
 
-      it "returns ['collection', 123]" do
-        expect(described_class.parse(href)).to eq([:collection, "123"])
+      it "can parse the resource and resource id" do
+        expect(described_class.new(href)).to have_attributes(:subject => :collection, :subject_id => "123")
       end
     end
 
     context "with a full url for the href and a API version" do
       let(:href) { "http://localhost:3000/api/v1.2.3/collection/123" }
 
-      it "returns ['collection', 123]" do
-        expect(described_class.parse(href)).to eq([:collection, "123"])
+      it "can parse the resource and resource id" do
+        expect(described_class.new(href)).to have_attributes(:subject => :collection, :subject_id => "123")
       end
     end
 
     context "with a partial URL that starts with '/api/...'" do
       let(:href) { "/api/collection/123" }
 
-      it "returns ['collection', 123]" do
-        expect(described_class.parse(href)).to eq([:collection, "123"])
+      it "can parse the resource and resource id" do
+        expect(described_class.new(href)).to have_attributes(:subject => :collection, :subject_id => "123")
       end
     end
 
     context "with a partial URL that starts with '/api/v1.2.3/...'" do
       let(:href) { "/api/v1.2.3/collection/123" }
 
-      it "returns ['collection', 123]" do
-        expect(described_class.parse(href)).to eq([:collection, "123"])
+      it "can parse the resource and resource id" do
+        expect(described_class.new(href)).to have_attributes(:subject => :collection, :subject_id => "123")
       end
     end
 
     context "with a partial URL that starts with 'api/...'" do
       let(:href) { "api/collection/123" }
 
-      it "returns ['collection', 123]" do
-        expect(described_class.parse(href)).to eq([:collection, "123"])
+      it "can parse the resource and resource id" do
+        expect(described_class.new(href)).to have_attributes(:subject => :collection, :subject_id => "123")
       end
     end
 
     context "with a partial URL that starts with 'api/v1.2.3/...'" do
       let(:href) { "api/v1.2.3/collection/123" }
 
-      it "returns ['collection', 123]" do
-        expect(described_class.parse(href)).to eq([:collection, "123"])
+      it "can parse the resource and resource id" do
+        expect(described_class.new(href)).to have_attributes(:subject => :collection, :subject_id => "123")
       end
     end
 
     context "with a partial URL without '/api/...'" do
       let(:href) { "collection/123" }
 
-      it "returns ['collection', 123]" do
-        expect(described_class.parse(href)).to eq([:collection, "123"])
+      it "can parse the resource and resource id" do
+        expect(described_class.new(href)).to have_attributes(:subject => :collection, :subject_id => "123")
       end
     end
 
     context "with a partial URL without '/api/...' and a leading slash" do
       let(:href) { "/collection/123" }
 
-      it "returns ['collection', 123]" do
-        expect(described_class.parse(href)).to eq([:collection, "123"])
+      it "can parse the resource and resource id" do
+        expect(described_class.new(href)).to have_attributes(:subject => :collection, :subject_id => "123")
       end
     end
   end
