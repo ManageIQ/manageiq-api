fed2730bde81b8e897524c5a6e906403dc6eb84b
Tzu-Mainn Chen <tzumainn@redhat.com>
2017-10-05 21:27:41 +0200

    remove string freeze

--- app/controllers/api/subcollections/security_groups.rb
+++ app/controllers/api/subcollections/security_groups.rb
@@ -2,7 +2,7 @@ module Api
   module Subcollections
     module SecurityGroups
       def security_groups_query_resource(object)
-        object.respond_to?(:security_groups) ? object.security_groups : [].freeze
+        object.respond_to?(:security_groups) ? object.security_groups : []
       end
     end
   end
