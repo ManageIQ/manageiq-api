a30a44da5dd26e06b5b0a614179697af2fa08603
mkanoor <mkanoor@redhat.com>
2017-10-11 12:03:19 -0400

    Fixed typo

--- app/controllers/api/subcollections/request_tasks.rb
+++ app/controllers/api/subcollections/request_tasks.rb
@@ -7,7 +7,7 @@ module Api
       end
 
       def request_tasks_edit_resource(_object, type, id = nil, data = {})
-        raise BadRequestError, "Must specify a id for editing a #{type} resource" unless id
+        raise BadRequestError, "Must specify an id for editing a #{type} resource" unless id
         request_task = resource_search(id, type, collection_class(:request_tasks))
         request_task.update_request_task(data)
         request_task
