6e265f3d0acd4de2ac786594492a78c0154f8638
Jillian Tullo <jtullo@redhat.com>
2017-10-16 14:15:26 -0400

    return image_href and extension by default

--- app/controllers/api/pictures_controller.rb
+++ app/controllers/api/pictures_controller.rb
@@ -1,9 +1,17 @@
 module Api
   class PicturesController < BaseController
+    before_action :set_additional_attributes, :only => [:index, :show]
+
     def create_resource(_type, _id, data)
       Picture.create_from_base64(data)
     rescue => err
       raise BadRequestError, "Failed to create Picture - #{err}"
+    end
+
+    private
+
+    def set_additional_attributes
+      @additional_attributes = %w(image_href extension)
     end
   end
 end
