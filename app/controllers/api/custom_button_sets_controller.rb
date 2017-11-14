47de18d3fc67b9bd003fca95c8153d0e160a1f24
Aparna Karve <akarve@redhat.com>
2017-11-14 13:48:00 -0800

    Add OPTIONS for Custom Button Sets
    to retrieve all Custom Button Sets for -
    GenericObjectDefinition and ServiceTemplate

--- app/controllers/api/custom_button_sets_controller.rb
+++ app/controllers/api/custom_button_sets_controller.rb
@@ -7,5 +7,18 @@ module Api
     def edit_resource(type, id, data)
       super(type, id, data.deep_symbolize_keys)
     end
+
+    def options
+      render_options(:custom_button_sets, build_custom_button_sets_options)
+    end
+
+    def build_custom_button_sets_options
+      {
+        :custom_button_sets => {
+          :generic_object_definitions => CustomButtonSet.find_all_by_class_name('GenericObjectDefinition'),
+          :service_templates          => CustomButtonSet.find_all_by_class_name('ServiceTemplate')
+        }
+      }
+    end
   end
 end
