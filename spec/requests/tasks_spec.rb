fa77ac07b4de3c6738aa830ccd350c03063ef9cf
Martin Hradil <mhradil@redhat.com>
2017-11-29 15:48:41 +0000

    tasks_spec - shut up miq-bot
    apparently miq-bot complains about readable hashes - moving back to vars

--- spec/requests/tasks_spec.rb
+++ spec/requests/tasks_spec.rb
@@ -20,38 +20,42 @@ describe 'TasksController' do
   it 'deletes on POST' do
     api_basic_authorize resource_action_identifier(:tasks, :delete)
 
-    post(api_task_url(nil, task.id), :params => {
+    data = {
       :action => 'delete'
-    })
+    }
+    post(api_task_url(nil, task.id), :params => data)
 
     expect(response).to have_http_status(:ok) # 200
     expect_deleted(task)
 
-    expect(response.parsed_body).to include({
+    expected = {
       'success' => true,
       'message' => "tasks id: #{task.id} deleting"
-    })
+    }
+    expect(response.parsed_body).to include(expected)
   end
 
   it 'bulk deletes' do
     api_basic_authorize collection_action_identifier(:tasks, :delete)
 
-    post(api_tasks_url, :params => {
+    data = {
       :action    => 'delete',
       :resources => [
         {:href => api_task_url(nil, task.id)},
         {:href => api_task_url(nil, task2.id)}
       ]
-    })
+    }
+    post(api_tasks_url, :params => data)
 
     expect(response).to have_http_status(:ok) # 200
     expect_deleted(task, task2)
 
-    expect(response.parsed_body).to include({
+    expected = {
       'results' => a_collection_including(
         a_hash_including('success' => true, 'message' => "tasks id: #{task.id} deleting"),
         a_hash_including('success' => true, 'message' => "tasks id: #{task2.id} deleting")
       )
-    })
+    }
+    expect(response.parsed_body).to include(expected)
   end
 end
