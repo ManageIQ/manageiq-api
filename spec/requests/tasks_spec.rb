80834992e9cfb29b48b8cf37febf64d8ecdf2398
Alberto Bellotti <abellotti@users.noreply.github.com>
2017-12-05 13:17:36 -0500

    Merge pull request #220 from himdel/tasks-delete
    Tasks: delete support
    (cherry picked from commit 85f315312fff5ba1fb41bbc71fb833766272d49e)

---
+++ spec/requests/tasks_spec.rb
@@ -0,0 +1,61 @@
+describe 'TasksController' do
+  let(:task) { FactoryGirl.create(:miq_task, :state => MiqTask::STATE_FINISHED) }
+  let(:task2) { FactoryGirl.create(:miq_task, :state => MiqTask::STATE_FINISHED) }
+
+  def expect_deleted(*args)
+    args.each do |arg|
+      expect(MiqTask.find_by(:id => arg.id)).to be_nil
+    end
+  end
+
+  it 'deletes on DELETE' do
+    api_basic_authorize resource_action_identifier(:tasks, :delete, :delete)
+
+    delete(api_task_url(nil, task))
+
+    expect(response).to have_http_status(:no_content) # 204
+    expect_deleted(task)
+  end
+
+  it 'deletes on POST' do
+    api_basic_authorize resource_action_identifier(:tasks, :delete)
+
+    data = {
+      :action => 'delete'
+    }
+    post(api_task_url(nil, task), :params => data)
+
+    expect(response).to have_http_status(:ok) # 200
+    expect_deleted(task)
+
+    expected = {
+      'success' => true,
+      'message' => "tasks id: #{task.id} deleting"
+    }
+    expect(response.parsed_body).to include(expected)
+  end
+
+  it 'bulk deletes' do
+    api_basic_authorize collection_action_identifier(:tasks, :delete)
+
+    data = {
+      :action    => 'delete',
+      :resources => [
+        {:href => api_task_url(nil, task)},
+        {:href => api_task_url(nil, task2)}
+      ]
+    }
+    post(api_tasks_url, :params => data)
+
+    expect(response).to have_http_status(:ok) # 200
+    expect_deleted(task, task2)
+
+    expected = {
+      'results' => a_collection_including(
+        a_hash_including('success' => true, 'message' => "tasks id: #{task.id} deleting"),
+        a_hash_including('success' => true, 'message' => "tasks id: #{task2.id} deleting")
+      )
+    }
+    expect(response.parsed_body).to include(expected)
+  end
+end
