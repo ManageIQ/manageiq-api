73e78ef861865d1acbed4d7a71b3382dcfc7e5cc
Erez Freiberger <efreiber@redhat.com>
2018-01-08 11:26:02 +0200

    adding 'scan_report_summary' action to container images
    The action `scan_report_summary` will return a list of all the openscap
    rules  of the image with their result and severity.
    
    Fixes https://bugzilla.redhat.com/show_bug.cgi?id=1446699

--- app/controllers/api/container_images_controller.rb
+++ app/controllers/api/container_images_controller.rb
@@ -1,5 +1,14 @@
 module Api
   class ContainerImagesController < BaseController
+    def openscap_scan_results_resource(type, id = nil, _data = nil)
+      raise BadRequestError, "Must specify an id for getting OpenScap results of a #{type} resource" unless id
+      api_action(type, id) do |klass|
+        cimage = resource_search(id, type, klass)
+        summary = cimage.openscap_rule_results
+        action_result(true, "summary" => summary)
+      end
+    end
+
     def scan_resource(type, image_id, _payload)
       raise BadRequestError, "Must specify an id for starting a #{type} resource" unless image_id
       api_action(type, image_id, :skip_href => true) do |klass|
