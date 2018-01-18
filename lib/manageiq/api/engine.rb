d3fc80e5a2278fc48ea17608dccdf6942990608d
Nick LaMuro <nicklamuro@gmail.com>
2018-01-18 13:03:05 -0600

    Add comment about leak in engine.rb

--- lib/manageiq/api/engine.rb
+++ lib/manageiq/api/engine.rb
@@ -3,6 +3,15 @@ module ManageIQ
   module Api
     class Engine < ::Rails::Engine
       isolate_namespace ManageIQ::Api
+
+      # NOTE:  If you are going to make changes to autoload_paths, please make
+      # sure they are all strings.  Rails will push these paths into the
+      # $LOAD_PATH.
+      #
+      # More info can be found in the ruby-lang bug:
+      #
+      #   https://bugs.ruby-lang.org/issues/14372
+      #
       config.autoload_paths << root.join('app', 'controllers', 'api').to_s
       config.autoload_paths << root.join('lib', 'services').to_s
       config.autoload_paths << root.join('lib').to_s
