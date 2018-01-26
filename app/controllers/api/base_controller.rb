6dd2ba83e40a8f0f81f0d4f603ed536847c6babb
Jillian Tullo <jtullo@redhat.com>
2018-01-26 08:59:45 -0500

    Allow an array to be specified on PATCH for settings
    The rest of the API supports an array being passed to PATCH, and this will allow it to remain consistent with what is accepted elsewhere. In addition, it supports multiple updates to the settings at once.

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -117,7 +117,13 @@ module Api
       case @req.method
       when :patch
         raise ForbiddenError, "You are not authorized to edit settings." unless super_admin?
-        resource.add_settings_for_resource(@req.json_body)
+        if @req.json_body.kind_of?(Array)
+          @req.json_body.each do |setting|
+            resource.add_settings_for_resource(setting)
+          end
+        else
+          resource.add_settings_for_resource(@req.json_body)
+        end
       when :delete
         raise ForbiddenError, "You are not authorized to remove settings." unless super_admin?
         resource.remove_settings_path_for_resource(*@req.json_body)
