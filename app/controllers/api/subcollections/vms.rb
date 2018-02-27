6544c05b67ece861044a63e19b72f7f3a6d77b52
Jillian Tullo <jtullo@redhat.com>
2018-02-27 09:21:41 -0500

    Remove unused create_vm_attributes_hash method

--- app/controllers/api/subcollections/vms.rb
+++ app/controllers/api/subcollections/vms.rb
@@ -12,14 +12,6 @@ module Api
           vm.as_json.merge(attributes_hash)
         end
       end
-
-      private
-
-      def create_vm_attributes_hash(vm_attrs, vm)
-        vm_attrs.each_with_object({}) do |attr, hash|
-          hash[attr] = vm.public_send(attr.to_sym) if vm.respond_to?(attr.to_sym)
-        end.compact
-      end
     end
   end
 end
