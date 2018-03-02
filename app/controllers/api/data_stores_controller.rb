4e59f2aa3b183be7fbc7c01781aea6926bd56d00
Alberto Bellotti <abellott@redhat.com>
2018-03-02 07:55:07 -0500

    Added support for deleting data_stores
      DELETE /api/data_stores/:id
      POST /api/data_stores/:id   - action "delete"
      POST /api/data_stores       - bulk action "delete"
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1511376

--- app/controllers/api/data_stores_controller.rb
+++ app/controllers/api/data_stores_controller.rb
@@ -1,5 +1,21 @@
 module Api
   class DataStoresController < BaseController
     include Subcollections::Tags
+
+    def delete_resource(type, id = nil, _data = nil)
+      delete_action_handler do
+        data_store = resource_search(id, type, collection_class(type))
+        desc = "Deleting #{data_store_ident(data_store)}"
+        api_log_info(desc)
+        task_id = queue_object_action(data_store, desc, :method_name => "destroy")
+        action_result(true, desc, :task_id => task_id, :parent_id => id)
+      end
+    end
+
+    private
+
+    def data_store_ident(data_store)
+      "Data Store id:#{data_store.id} name:'#{data_store.name}'"
+    end
   end
 end
