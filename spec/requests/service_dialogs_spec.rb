a7388e37f5bf39e975c3ee50dc8a981434a8f66c
Gregg Tanzillo <gtanzill@redhat.com>
2018-03-08 12:33:17 -0500

    Merge pull request #343 from jntullo/bz_1546944
    Add svc_catalog_provision product feature to service dialog queries
    (cherry picked from commit 31b312c741321ca71e98bac76abf8224ffac53e2)
    
    Fixes https://bugzilla.redhat.com/show_bug.cgi?id=1553809

--- spec/requests/service_dialogs_spec.rb
+++ spec/requests/service_dialogs_spec.rb
@@ -30,6 +30,22 @@ describe "Service Dialogs API" do
       expect(response).to have_http_status(:ok)
     end
 
+    it "allows read of service dialogs with the service catalog provision role" do
+      api_basic_authorize("svc_catalog_provision")
+
+      get api_service_dialogs_url
+
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "allows read of a single service dialog with the service catalog provision role" do
+      api_basic_authorize("svc_catalog_provision")
+
+      get api_service_dialog_url(nil, dialog1)
+
+      expect(response).to have_http_status(:ok)
+    end
+
     it "query with expanded resources to include content" do
       api_basic_authorize collection_action_identifier(:service_dialogs, :read, :get)
       get api_service_dialogs_url, :params => { :expand => "resources" }
