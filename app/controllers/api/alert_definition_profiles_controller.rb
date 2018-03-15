09a440a7ccb516510716baee68d9ee3250f554b0
Jillian Tullo <jtullo@redhat.com>
2018-03-15 15:55:33 -0400

    Unassign alert definition profiles
    Fixes https://bugzilla.redhat.com/show_bug.cgi?id=1539379

--- app/controllers/api/alert_definition_profiles_controller.rb
+++ app/controllers/api/alert_definition_profiles_controller.rb
@@ -40,6 +40,24 @@ module Api
       action_result(false, "Could not assign Alert Definition Profile - #{err}")
     end
 
+    def unassign_resource(type, id, data)
+      profile = resource_search(id, type, collection_class(type))
+      data['resources'].each do |resource|
+        href = Href.new(resource['href'])
+        if resource.key?('tag')
+          tag_id = Href.new(resource['tag']['href']).subject_id
+          tag = resource_search(tag_id, :tags, collection_class(:tags))
+          profile.unassign_tags([tag.classification], href.subject)
+        else
+          assignable_resource = resource_search(href.subject_id, href.subject, collection_class(href.subject))
+          profile.unassign_objects([assignable_resource])
+        end
+      end
+      action_result(true, "Unassigned resources from #{alert_definition_profile_ident(profile)}")
+    rescue => err
+      action_result(false, "Could not unassign Alert Definition Profile - #{err}")
+    end
+
     private
 
     def alert_definition_profile_ident(profile)
--- spec/requests/alert_definition_profiles_spec.rb
+++ spec/requests/alert_definition_profiles_spec.rb
@@ -26,6 +26,34 @@ describe "Alert Definition Profiles API" do
         expect(response.parsed_body).to include(expected)
       end
     end
+
+    context "unassign" do
+      it "assigns alert profiles with an appropriate role" do
+        api_basic_authorize(collection_action_identifier(:alert_definition_profiles, :edit))
+        alert_definition_profile = FactoryGirl.create(:miq_alert_set)
+        cluster = FactoryGirl.create(:ems_cluster)
+        dept = FactoryGirl.create(:classification_department)
+        classification = FactoryGirl.create(:classification_tag, :name => "foo", :parent => dept)
+        alert_definition_profile.assign_to_tags([classification], "host")
+
+        request = {
+          "action"    => "unassign",
+          "resources" => [
+            { "id" => alert_definition_profile.id, "resources" => [
+              {"href" => api_hosts_url, "tag" => { "href" => api_tag_url(nil, classification.tag) }},
+              {"href" => api_cluster_url(nil, cluster) }
+            ]}
+          ]
+        }
+        post(api_alert_definition_profiles_url, :params => request)
+
+        expected = {
+          "results" => [{"success" => true, "message" => /Unassigned resources from Alert Definition Profile/}]
+        }
+        expect(response).to have_http_status(:ok)
+        expect(response.parsed_body).to include(expected)
+      end
+    end
   end
 
   describe "POST /api/alert_definition_profiles" do
@@ -48,6 +76,30 @@ describe "Alert Definition Profiles API" do
 
         expect(response).to have_http_status(:ok)
         expect(response.parsed_body).to include("success" => true, "message" => /Assigned resources to Alert Definition Profile/)
+      end
+    end
+
+    context "unassign" do
+      it "unassigns objects and tags with an appropriate role" do
+        api_basic_authorize(collection_action_identifier(:alert_definition_profiles, :edit))
+        alert_definition_profile = FactoryGirl.create(:miq_alert_set)
+        cluster = FactoryGirl.create(:ems_cluster)
+        alert_definition_profile.assign_to_objects([cluster])
+        dept = FactoryGirl.create(:classification_department)
+        classification = FactoryGirl.create(:classification_tag, :name => "foo", :parent => dept)
+        alert_definition_profile.assign_to_tags([classification], "host")
+
+        request = {
+          "action"    => "unassign",
+          "resources" => [
+            {"href" => api_hosts_url, "tag" => { "href" => api_tag_url(nil, classification.tag) }},
+            {"href" => api_cluster_url(nil, cluster) }
+          ]
+        }
+        post(api_alert_definition_profile_url(nil, alert_definition_profile), :params => request)
+
+        expect(response).to have_http_status(:ok)
+        expect(response.parsed_body).to include("success" => true, "message" => /Unassigned resources from Alert Definition Profile/)
       end
     end
   end
