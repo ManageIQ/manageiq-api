f7e0d4eda221913a1bba532d78e7f56b17092b2b
Tim Wade <hello@timjwade.com>
2018-05-01 09:45:00 -0700

    Delegate settings show fetch to settings filterer

--- app/controllers/api/settings_controller.rb
+++ app/controllers/api/settings_controller.rb
@@ -5,7 +5,7 @@ module Api
     end
 
     def show
-      settings_value = entry_value(whitelist_settings(settings_hash), @req.c_suffix)
+      settings_value = entry_value(SettingsFilterer.filter_for(current_user), @req.c_suffix)
 
       raise NotFoundError, "Settings entry #{@req.c_suffix} not found" if settings_value.nil?
       render_resource :settings, settings_entry_to_hash(@req.c_suffix, settings_value)
@@ -51,20 +51,6 @@ module Api
       def settings_entry_to_hash(path, value)
         {}.tap { |h| h.store_path(path.split("/"), value) }
       end
-    end
-
-    def whitelist_settings(settings)
-      return settings if super_admin?
-
-      result_hash = {}
-      ApiConfig.collections[:settings][:categories].each do |category_path|
-        result_hash.deep_merge!(settings_entry_to_hash(category_path, entry_value(settings, category_path)))
-      end
-      result_hash
-    end
-
-    def settings_hash
-      @settings_hash ||= Settings.to_hash.deep_stringify_keys
     end
 
     def entry_value(settings, path)
