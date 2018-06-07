1d52463fda0ef30330f99e25511c825d54bbacb6
Douglas Gabriel <douglasxgabriel@gmail.com>
2018-06-07 10:47:18 -0300

    Adding Physical Switch power action

--- spec/requests/physical_switches_spec.rb
+++ spec/requests/physical_switches_spec.rb
@@ -1,7 +1,10 @@
 describe "Physical Switches API" do
+  let(:physical_switch) { FactoryGirl.create(:physical_switch) }
+
   context "GET /api/physical_switches" do
     it "returns all Physical Switches" do
-      physical_switch = FactoryGirl.create(:physical_switch)
+      physical_switch
+
       api_basic_authorize('physical_switch_show_list')
 
       get(api_physical_switches_url)
@@ -17,7 +20,6 @@ describe "Physical Switches API" do
 
   context "GET /api/physical_switches/:id" do
     it "returns a single Physical Switch" do
-      physical_switch = FactoryGirl.create(:physical_switch)
       api_basic_authorize('physical_switch_show')
 
       get(api_physical_switch_url(nil, physical_switch))
@@ -44,7 +46,6 @@ describe "Physical Switches API" do
 
     context "without an appropriate role" do
       it "it responds with 403 Forbidden" do
-        physical_switch = FactoryGirl.create(:physical_switch)
         api_basic_authorize
 
         post(api_physical_switch_url(nil, physical_switch), :params => gen_request(:refresh))
@@ -63,7 +64,6 @@ describe "Physical Switches API" do
       end
 
       it "refresh of a single Physical Switch" do
-        physical_switch = FactoryGirl.create(:physical_switch)
         api_basic_authorize('physical_switch_refresh')
 
         post(api_physical_switch_url(nil, physical_switch), :params => gen_request(:refresh))
@@ -94,6 +94,81 @@ describe "Physical Switches API" do
         }
         expect(response.parsed_body).to include(expected)
         expect(response).to have_http_status(:ok)
+      end
+    end
+  end
+
+  describe "Physical Switches restart action" do
+    let(:action) { :restart }
+
+    context "For multiple resources" do
+      context "With an invalid id and valid id" do
+        it "returns a HTTP status 200, perform the action for the valid resource and fail for the invalid" do
+          api_basic_authorize(action_identifier(:physical_switches, action, :resource_actions, :post))
+
+          post(api_physical_switches_url, :params => gen_request(action, [{"href" => api_physical_switch_url(nil, physical_switch)}, {"href" => api_physical_switch_url(nil, 999_999)}]))
+
+          expected = {
+            "results" => a_collection_containing_exactly(
+              a_hash_including(
+                "message" => a_string_matching("Performing #{action} for Physical Switch id:#{physical_switch.id} name: '#{physical_switch.name}'"),
+                "success" => true,
+                "href"    => api_physical_switch_url(nil, physical_switch)
+              ),
+              a_hash_including(
+                "success" => false
+              )
+            )
+          }
+          expect(response.parsed_body).to include(expected)
+          expect(response).to have_http_status(:ok)
+        end
+      end
+
+      context "With a valid request" do
+        it "performs the action succesfully" do
+          api_basic_authorize(action_identifier(:physical_switches, action, :resource_actions, :post))
+
+          post(api_physical_switches_url, :params => gen_request(action, [{"href" => api_physical_switch_url(nil, physical_switch)}]))
+
+          expected = {
+            "results" => a_collection_containing_exactly(
+              a_hash_including(
+                "message" => a_string_matching("Performing #{action} for Physical Switch id:#{physical_switch.id} name: '#{physical_switch.name}'"),
+                "success" => true,
+                "href"    => api_physical_switch_url(nil, physical_switch)
+              )
+            )
+          }
+          expect(response.parsed_body).to include(expected)
+          expect(response).to have_http_status(:ok)
+        end
+      end
+    end
+
+    context "For a single resource" do
+      context "With an invalid id and valid id" do
+        it "it responds with 404 Not Found" do
+          api_basic_authorize(action_identifier(:physical_switches, action, :resource_actions, :post))
+
+          post(api_physical_switch_url(nil, 999_999), :params => gen_request(action))
+
+          expect(response).to have_http_status(:not_found)
+        end
+      end
+
+      context "With a valid request" do
+        it "restarts the Physical Switch" do
+          api_basic_authorize(action_identifier(:physical_switches, action, :resource_actions, :post))
+
+          post(api_physical_switch_url(nil, physical_switch), :params => gen_request(action))
+
+          expect_single_action_result(
+            :success => true,
+            :message => a_string_matching("Performing #{action} for Physical Switch id:#{physical_switch.id} name: '#{physical_switch.name}'"),
+            :href    => api_physical_switch_url(nil, physical_switch)
+          )
+        end
       end
     end
   end
