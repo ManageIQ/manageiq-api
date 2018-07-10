fdce501c126b27d95607b9bc3e20738f8f1b3775
Brandon Dunne <brandondunne@hotmail.com>
2018-07-10 17:29:51 -0400

    Allow ServiceTemplate Schedule updates
    https://bugzilla.redhat.com/show_bug.cgi?id=1564255

--- app/controllers/api/subcollections/schedules.rb
+++ app/controllers/api/subcollections/schedules.rb
@@ -9,6 +9,13 @@ module Api
         delete_resource(type, id, data)
       end
       alias delete_resource_schedules schedules_delete_resource
+
+      def schedules_edit_resource(_parent, _type, id, data)
+        # We need to hit #edit_resource from the BaseController, not any of the override methods in child controllers
+        BaseController.instance_method(:edit_resource).bind(self).call(:schedules, id, data)
+      rescue => err
+        raise BadRequestError, "Could not update Schedule - #{err}"
+      end
     end
   end
 end
