ddd68ec22f92c9475d4ed6f15132cdd934da00ea
Brandon Dunne <brandondunne@hotmail.com>
2018-07-10 15:03:00 -0400

    MiqReport schedule queries are unique to that class
    Move that lookup into reports controller
    Move the generic schedule query and delete logic into the schedules subcollection mixin

--- app/controllers/api/subcollections/schedules.rb
+++ app/controllers/api/subcollections/schedules.rb
@@ -2,8 +2,13 @@ module Api
   module Subcollections
     module Schedules
       def schedules_query_resource(object)
-        object ? object.list_schedules : {}
+        object.miq_schedules
       end
+
+      def schedules_delete_resource(_parent, type, id, data)
+        delete_resource(type, id, data)
+      end
+      alias delete_resource_schedules schedules_delete_resource
     end
   end
 end
