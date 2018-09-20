99221804f4d0cc8688dfb934f88be2d6b47f1b73
Brandon Dunne <brandondunne@hotmail.com>
2018-09-19 23:20:51 -0400

    Refactor into vms and instances shared_examples
    https://bugzilla.redhat.com/show_bug.cgi?id=1628658

--- spec/requests/central_admin/instances_spec.rb
+++ spec/requests/central_admin/instances_spec.rb
@@ -1,29 +1,12 @@
 describe "/api/instances central admin" do
-  let(:api_connection)           { double("ApiClient", :instances => api_instances_collection) }
-  let(:api_instance)             { double("Instance") }
-  let(:api_instances_collection) { double("/api/instances") }
-  let(:region_remote)            { FactoryGirl.create(:miq_region) }
-  let!(:vm_remote)               { FactoryGirl.create(:vm_amazon, :id => ApplicationRecord.id_in_region(1, region_remote.region)) }
+  let(:resource_type) { "instance" }
 
-  shared_examples "basic operations" do |operation|
-    let(:operation) { operation }
-    it operation.to_s do
-      api_basic_authorize(action_identifier(:instances, operation))
-
-      expect(InterRegionApiMethodRelay).to receive(:api_client_connection_for_region).with(region_remote.region).and_return(api_connection)
-      expect(api_instances_collection).to receive(:find).with(vm_remote.id).and_return(api_instance)
-      expect(api_instance).to receive(operation)
-
-      post(api_instance_url(nil, vm_remote), :params => gen_request(operation))
-    end
-  end
-
-  include_examples "basic operations", :pause
-  include_examples "basic operations", :reboot_guest
-  include_examples "basic operations", :reset
-  include_examples "basic operations", :shelve
-  include_examples "basic operations", :start
-  include_examples "basic operations", :stop
-  include_examples "basic operations", :suspend
-  include_examples "basic operations", :terminate
+  include_examples "resource power operations", :vm_amazon, :pause
+  include_examples "resource power operations", :vm_amazon, :reboot_guest
+  include_examples "resource power operations", :vm_amazon, :reset
+  include_examples "resource power operations", :vm_amazon, :shelve
+  include_examples "resource power operations", :vm_amazon, :start
+  include_examples "resource power operations", :vm_amazon, :stop
+  include_examples "resource power operations", :vm_amazon, :suspend
+  include_examples "resource power operations", :vm_amazon, :terminate
 end
