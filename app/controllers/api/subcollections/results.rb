f9ad6842a516bc3c9577d7c3945e0c48da08cfdc
Pekka Wallendahl <wyrmiyu@gmail.com>
2018-12-03 13:17:36 +0200

    Remove fetch_direct_virtual_attribute() from subcollections

--- app/controllers/api/subcollections/results.rb
+++ app/controllers/api/subcollections/results.rb
@@ -1,22 +1,12 @@
 module Api
   module Subcollections
     module Results
-      def initialize
-        @results_controller = Api::ResultsController.new
-      end
-
       def find_results(id)
         MiqReportResult.for_user(User.current_user).find(id)
       end
 
       def results_query_resource(object)
         object.miq_report_results.for_user(User.current_user)
-      end
-
-      private
-
-      def fetch_direct_virtual_attribute(type, resource, attr)
-        @results_controller.fetch_direct_virtual_attribute(type, resource, attr)
       end
     end
   end
