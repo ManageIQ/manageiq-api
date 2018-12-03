7cc9b03ac8e90cfa0db8852124e2d79c1eaa7ef9
Pekka Wallendahl <wyrmiyu@gmail.com>
2018-12-03 04:32:29 +0200

    Fix missing end clauses

--- app/controllers/api/base_controller/renderer.rb
+++ app/controllers/api/base_controller/renderer.rb
@@ -263,10 +263,12 @@ module Api
       def fetch_direct_virtual_attribute(type, resource, attr)
         unless attr_accessible?(resource, attr)
           return
+        end
         virtattr_accessor = virtual_attribute_accessor(type, attr)
         value = virtattr_accessor ? send(virtattr_accessor, resource) : virtual_attribute_search(resource, attr)
         if attr == "custom_actions"
           value = add_custom_action_hrefs(value)
+        end
         if attr == "result_set"
           offset = request.params[:offset] ? request.params[:offset].to_i : 0
           limit = request.params[:limit] ? request.params[:limit].to_i : 0
