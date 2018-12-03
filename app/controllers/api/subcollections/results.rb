a62a51a063cf012f600ca82e9f7b8d409c91a962
Pekka Wallendahl <wyrmiyu@gmail.com>
2018-12-03 04:30:29 +0200

    Refactor fetch_direct_virtual_attribute()

--- app/controllers/api/subcollections/results.rb
+++ app/controllers/api/subcollections/results.rb
@@ -1,6 +1,10 @@
 module Api
   module Subcollections
     module Results
+      def initialize()
+        @results_controller = Api::ResultsController.new
+      end
+
       def find_results(id)
         MiqReportResult.for_user(User.current_user).find(id)
       end
@@ -12,18 +16,7 @@ module Api
       private
 
       def fetch_direct_virtual_attribute(type, resource, attr)
-        return unless attr_accessible?(resource, attr)
-        virtattr_accessor = virtual_attribute_accessor(type, attr)
-        value = virtattr_accessor ? send(virtattr_accessor, resource) : virtual_attribute_search(resource, attr)
-        value = add_custom_action_hrefs(value) if attr == "custom_actions"
-        if attr == "result_set"
-          offset = request.params[:offset] ? request.params[:offset].to_i : 0
-          limit = request.params[:limit] ? request.params[:limit].to_i : 0
-          value = value.sort_by{|hash| hash[:id] }[offset...offset+limit] if limit > 0
-        end
-        result = {attr => normalize_attr(attr, value)}
-        # set nil vtype above to "#{type}/#{resource.id}/#{attr}" to support id normalization
-        [value, result]
+        @results_controller.fetch_direct_virtual_attribute(type, resource, attr)
       end
     end
   end
