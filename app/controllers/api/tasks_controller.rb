fed05060167166fb7dff904e2a24872b2dfbc789
Joe VLcek <jvlcek@redhat.com>
2018-12-21 17:17:37 -0500

    Super baby super

--- app/controllers/api/tasks_controller.rb
+++ app/controllers/api/tasks_controller.rb
@@ -3,13 +3,13 @@ module Api
     def find_collection(klass)
       return klass.where(:userid => [current_user.userid]) if current_user.only_my_user_tasks?
 
-      klass.all
+      super
     end
 
     def find_resource(klass, key_id, id)
       return klass.find_by(key_id => id, :userid => [current_user.userid]) if current_user.only_my_user_tasks?
 
-      key_id == "id" ? klass.find(id) : klass.find_by(key_id => id)
+      super
     end
   end
 end
