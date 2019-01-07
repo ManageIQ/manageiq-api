06e7c4961c1285c7f689bbbfcff409e558d24bcd
Joe VLcek <jvlcek@redhat.com>
2019-01-07 15:56:49 -0500

    Update SQL regex matching to include UPDATE, INSERT and DELETE

--- lib/services/api/error_serializer.rb
+++ lib/services/api/error_serializer.rb
@@ -2,12 +2,14 @@ module Api
   class ErrorSerializer
     attr_reader :kind, :error
 
+    SQL_STATEMENTS = " SELECT | UPDATE | INSERT | DELETE ".freeze
+
     def initialize(kind, error)
       @kind = kind
       @error = error
     end
 
-    def serialize(remove_sql_select = false)
+    def serialize(no_sql_query = false)
       result = {
         :error => {
           :kind    => kind,
@@ -15,9 +17,16 @@ module Api
           :klass   => error.class.name
         }
       }
-      result[:error][:message] = error.message.split(/ SELECT /i)[0] if remove_sql_select
+      result[:error][:message] = remove_sql_query(error.message) if no_sql_query
       result[:error][:backtrace] = error.backtrace.join("\n") if Rails.env.test?
       result
+    end
+
+    private
+
+    def remove_sql_query(message)
+      return message unless message =~ /PG.*ERROR/i
+      message.split(/#{SQL_STATEMENTS}/i)[0]
     end
   end
 end
--- spec/lib/services/api/error_serializer_spec.rb
+++ spec/lib/services/api/error_serializer_spec.rb
@@ -1,20 +1,58 @@
 RSpec.describe Api::ErrorSerializer do
-  let(:message_without_sql_select) { "Error" }
-  let(:message_with_sql_select) { "#{message_without_sql_select} SELECT  \"users\".* FROM \"users\"" }
-  let(:error) { double("error", :message => message_with_sql_select, :backtrace => []) }
+  let(:non_sql_message) { "Not a PostgreSQL error" }
+  let(:non_sql_error) { double("non sql error", :message => non_sql_message, :backtrace => []) }
+
+  let(:pg_message_header) { "PG::Error" }
+
+  let(:pg_select_error) { "#{pg_message_header} SeLeCt  \"users\".* FROM \"users\"" }
+  let(:select_error) { double("select error", :message => pg_select_error, :backtrace => []) }
+
+  let(:pg_update_error) { "#{pg_message_header} UpDate  \"users\".* FROM \"users\"" }
+  let(:update_error) { double("update error", :message => pg_update_error, :backtrace => []) }
+
+  let(:pg_insert_error) { "#{pg_message_header} Insert  \"users\".* FROM \"users\"" }
+  let(:insert_error) { double("insert error", :message => pg_insert_error, :backtrace => []) }
+
+  let(:delete_error) { double("delete error", :message => pg_delete_error, :backtrace => []) }
+  let(:pg_delete_error) { "#{pg_message_header} dEleTe  \"users\".* FROM \"users\"" }
+
   let(:kind) { "kind" }
 
   describe ".serialize" do
-    it "returns a message with the SQL SELECT by default" do
-      actual = described_class.new(kind, error).serialize
+    it "returns the original message when not a PG error" do
+      actual = described_class.new(kind, non_sql_error).serialize
 
-      expect(actual[:error][:message]).to eq(message_with_sql_select)
+      expect(actual[:error][:message]).to eq(non_sql_message)
     end
 
-    it "returns a message without the SQL SELECT when requested" do
-      actual = described_class.new(kind, error).serialize(true)
+    it "returns a message with the SQL statement by default" do
+      actual = described_class.new(kind, select_error).serialize
 
-      expect(actual[:error][:message]).to eq(message_without_sql_select)
+      expect(actual[:error][:message]).to eq(pg_select_error)
+    end
+
+    it "returns a message without the SQL SELECT statement when requested" do
+      actual = described_class.new(kind, select_error).serialize(true)
+
+      expect(actual[:error][:message]).to eq(pg_message_header)
+    end
+
+    it "returns a message without the SQL UPDATE statement when requested" do
+      actual = described_class.new(kind, update_error).serialize(true)
+
+      expect(actual[:error][:message]).to eq(pg_message_header)
+    end
+
+    it "returns a message without the SQL INSERT statement when requested" do
+      actual = described_class.new(kind, insert_error).serialize(true)
+
+      expect(actual[:error][:message]).to eq(pg_message_header)
+    end
+
+    it "returns a message without the SQL DELETE statement when requested" do
+      actual = described_class.new(kind, delete_error).serialize(true)
+
+      expect(actual[:error][:message]).to eq(pg_message_header)
     end
   end
 end
