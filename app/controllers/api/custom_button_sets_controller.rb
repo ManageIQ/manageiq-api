e10b361bdb9b6b5fbd6bd139f1c52b187ddff842
Daniel Berger <dberger@redhat.com>
2019-01-14 13:05:28 -0500

    Include Subcollections::CustomButtons.

--- app/controllers/api/custom_button_sets_controller.rb
+++ app/controllers/api/custom_button_sets_controller.rb
@@ -1,5 +1,7 @@
 module Api
   class CustomButtonSetsController < BaseController
+    include Subcollections::CustomButtons
+
     def create_resource(type, id, data)
       super(type, id, data.deep_symbolize_keys)
     end
