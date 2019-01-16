c4b25d6de97865d325db8d8da262c2aa35aecacc
Daniel Berger <dberger@redhat.com>
2019-01-16 09:27:50 -0500

    Allow reads only for custom button subcollection.

--- app/controllers/api/subcollections/custom_buttons.rb
+++ app/controllers/api/subcollections/custom_buttons.rb
@@ -4,10 +4,6 @@ module Api
       def custom_buttons_query_resource(object)
         object.custom_buttons
       end
-
-      def custom_buttons_delete_resource(_object, type, id, data)
-        delete_resource(type, id, data)
-      end
     end
   end
 end
