4f333a8610d775d3278aab76f7f7ad5b30a4a531
Martin Marosi <marvusm.mmi@gmail.com>
2019-05-20 15:26:19 +0200

    Added create, update and delete actions to pxe servers.

--- app/controllers/api/pxe_servers_controller.rb
+++ app/controllers/api/pxe_servers_controller.rb
@@ -1,5 +1,60 @@
 module Api
   class PxeServersController < BaseController
+    INVALID_PXE_SERVER_ATTRS = %w(id href).freeze # Cannot update or create these
+
     include Subcollections::PxeImages
+    include Subcollections::PxeMenus
+
+    def create_resource(_type, _id, data = {})
+      validate_pxe_server_create_data(data)
+      menus = data.delete('pxe_menus')
+
+      server = collection_class(:pxe_servers).create(data)
+      if server.invalid?
+        raise BadRequestError, "Failed to add a pxe server - #{server.errors.full_messages.join(', ')}"
+      end
+      server.pxe_menus = create_pxe_menus(menus) if menus
+      server
+    end
+
+    def delete_resource(_type, id = nil, data = nil)
+      raise BadRequestError, "Must specify an id for deleting a pxe server" unless id
+      super
+    end
+
+
+    def edit_resource(type, id, data)
+      server = resource_search(id, type, collection_class(:pxe_servers))
+      
+
+      menus = data.delete('pxe_menus')
+      if menus
+        server.pxe_menus.clear
+        data.merge!('pxe_menus' => create_pxe_menus(menus))
+      end
+      server.update_attributes!(data)
+      server
+    end
+
+    private
+
+    def create_pxe_menus(menus)
+      menus.map do | menu |
+        collection_class(:pxe_menus).create(menu)
+      end
+    end
+
+    def validate_pxe_server_data(data)
+      bad_attrs = data.keys.select { |k| INVALID_PXE_SERVER_ATTRS.include?(k) }.compact.join(", ")
+      raise BadRequestError, "Invalid attribute(s) #{bad_attrs} specified for a pxe server" if bad_attrs.present?
+    end
+
+    def validate_pxe_server_create_data(data)
+      validate_pxe_server_data(data)
+      req_attrs = %w(name uri)
+      bad_attrs = []
+      req_attrs.each { |attr| bad_attrs << attr if data[attr].blank? }
+      raise BadRequestError, "Missing attribute(s) #{bad_attrs.join(', ')} for creating a pxe server" if bad_attrs.present?
+    end
   end
 end
--- spec/requests/pxe_servers_spec.rb
+++ spec/requests/pxe_servers_spec.rb
@@ -2,6 +2,7 @@ RSpec.describe 'PxeServers API' do
   let!(:pxe_server) { FactoryBot.create(:pxe_server) }
   let!(:pxe_image_1) { FactoryBot.create(:pxe_image, :pxe_server => pxe_server) }
   let!(:pxe_image_2) { FactoryBot.create(:pxe_image, :pxe_server => pxe_server) }
+  let!(:pxe_menu_1) { FactoryBot.create(:pxe_menu, :pxe_server => pxe_server) }
 
   describe 'GET /api/pxe_servers' do
     let(:url) { api_pxe_servers_url }
@@ -88,6 +89,105 @@ RSpec.describe 'PxeServers API' do
       api_basic_authorize
       get(url)
       expect(response).to have_http_status(:forbidden)
+    end
+  end
+
+  describe 'GET /api/pxe_servers/:id/pxe_menus' do
+    let(:url) { "/api/pxe_servers/#{pxe_server.id}/pxe_menus" }
+
+    it 'lists all pxe menus of a pxe server' do
+      api_basic_authorize subcollection_action_identifier(:pxe_servers, :pxe_menus, :read, :get)
+      get(url)
+      expect_result_resources_to_include_hrefs(
+        "resources",
+        [
+          api_pxe_server_pxe_menu_url(nil, pxe_server, pxe_menu_1)
+        ]
+      )
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'forbids access without an appropriate role' do
+      api_basic_authorize
+      get(url)
+      expect(response).to have_http_status(:forbidden)
+    end
+  end
+
+  describe 'GET /api/pxe_servers/:id/pxe_menus/:id' do
+    let(:url) { "/api/pxe_servers/#{pxe_server.id}/pxe_menus/#{pxe_menu_1.id}" }
+
+    it "will show a single pxe menu of a pxe server" do
+      api_basic_authorize subcollection_action_identifier(:pxe_servers, :pxe_images, :read, :get)
+      get(url)
+      expect_result_to_match_hash(
+        response.parsed_body,
+        "href"          => api_pxe_server_pxe_menu_url(nil, pxe_server, pxe_menu_1),
+        "id"            => pxe_menu_1.id.to_s,
+        "pxe_server_id" => pxe_server.id.to_s,
+        "file_name"          => pxe_menu_1.file_name
+      )
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'forbids access without an appropriate role' do
+      api_basic_authorize
+      get(url)
+      expect(response).to have_http_status(:forbidden)
+    end
+  end
+
+  describe 'POST /api/pxe_servers/' do
+    let(:url) { "/api/pxe_servers/" }
+
+    it 'create new pxe server' do
+      api_basic_authorize collection_action_identifier(:pxe_servers, :create, :post)
+      post(url, :params => {
+        :name => 'foo',
+        :uri  => 'bar/quax'
+      })
+      expect(response).to have_http_status(:ok)
+      expect(response.parsed_body['results'].first['name']).to eq('foo')
+      expect(response.parsed_body['results'].first['uri']).to eq('bar/quax')
+    end
+
+    it 'create new pxe server with pxe menu' do
+      api_basic_authorize collection_action_identifier(:pxe_servers, :create, :post)
+      post(url, :params => {
+        :name       => 'foo',
+        :uri        => 'bar/quax',
+        :pxe_menus  => [{:file_name => 'menu_1'}]
+      })
+      expect(response).to have_http_status(:ok)
+      expect(PxeServer.find(response.parsed_body['results'].first['id']).pxe_menus.first[:file_name]).to eq('menu_1')
+    end
+  end
+
+  describe 'patch /api/pxe_servers/:id' do
+    let(:url) { "/api/pxe_servers/#{pxe_server.id}" }
+
+    it 'update pxe server' do
+      api_basic_authorize collection_action_identifier(:pxe_servers, :edit, :patch)
+      patch(url, :params => {
+        :name       => 'updated name',
+        :uri        => 'updated/url',
+        :pxe_menus  => [{ file_name: 'updated menu' }]
+      })
+      expect(response).to have_http_status(:ok)
+      expect(response.parsed_body['name']).to eq('updated name')
+      expect(response.parsed_body['uri']).to eq('updated/url')
+      expect(PxeServer.find(response.parsed_body['id']).pxe_menus.first[:file_name]).to eq('updated menu')
+    end
+  end
+
+  describe 'delete /api/pxe_servers/:id' do
+    let(:url) { "/api/pxe_servers/#{pxe_server.id}" }
+
+    it 'delete pxe server' do
+      api_basic_authorize collection_action_identifier(:pxe_servers, :delete, :delete)
+      delete(url)
+      expect(response).to have_http_status(:no_content)
+      expect(PxeServer.exists?(pxe_server.id)).to be_falsey
     end
   end
 end
