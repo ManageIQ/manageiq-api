264701195902bc0f6d2f75756aa43b0b37f9e48a
Martin Marosi <marvusm.mmi@gmail.com>
2019-05-20 15:25:51 +0200

    Added pxe menu subcollection.

---
+++ app/controllers/api/pxe_menus_controller.rb
@@ -0,0 +1,4 @@
+module Api
+  class PxeMenusController < BaseController
+  end
+end
---
+++ app/controllers/api/subcollections/pxe_menus.rb
@@ -0,0 +1,9 @@
+module Api
+  module Subcollections
+    module PxeMenus
+      def pxe_menus_query_resource(object)
+        object.pxe_menus
+      end
+    end
+  end
+end
