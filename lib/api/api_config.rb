67e8dbc31af72526c0728e6960da57c94e2d933f
Jason Frey <jfrey@redhat.com>
2019-06-24 17:01:24 -0400

    Allow api.yml to be pluggable

--- lib/api/api_config.rb
+++ lib/api/api_config.rb
@@ -2,7 +2,10 @@ require "config"
 
 module Api
   ApiConfig = ::Config::Options.new.tap do |o|
-    o.add_source!(ManageIQ::Api::Engine.root.join("config/api.yml").to_s)
+    plugins = Vmdb::Plugins.all.dup
+    plugins.unshift(plugins.delete(ManageIQ::Api::Engine))
+    plugins.each { |p| o.add_source!(p.root.join("config/api.yml").to_s) }
+
     o.load!
   end
 end
