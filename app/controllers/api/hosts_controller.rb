78429487460711cc35078c7b3c490fc5ea8a1d30
Alexander Zagaynov <zalex80@gmail.com>
2019-07-24 16:26:48 +0200

    keys should be symbolized before merge

--- app/controllers/api/hosts_controller.rb
+++ app/controllers/api/hosts_controller.rb
@@ -15,8 +15,9 @@ module Api
       resource_search(id, type, collection_class(:hosts)).tap do |host|
         all_credentials = Array.wrap(credentials).each_with_object({}) do |creds, hash|
           auth_type = creds.delete(AUTH_TYPE_ATTR) || DEFAULT_AUTH_TYPE
+          creds.symbolize_keys!
           creds.reverse_merge!(:userid => host.authentication_userid(auth_type))
-          hash[auth_type.to_sym] = creds.symbolize_keys!
+          hash[auth_type.to_sym] = creds
         end
         host.update_authentication(all_credentials) if all_credentials.present?
       end
