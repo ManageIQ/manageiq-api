157a098538297208dac53b6cc1a32ecf2737c69a
Alberto Bellotti <abellott@redhat.com>
2019-08-08 18:57:06 -0400

    Adding support for resource action options that are controller driven and not api.yml driven.
    We needed the ability to override the default behavior of
    hiding encrypted attributes in responses for the refresh_dialog_field
    action when exercised on a resource.  This needed to be implemented
    without breaking any of the behavior for all other actions
    and queries.

--- app/controllers/api/service_dialogs_controller.rb
+++ app/controllers/api/service_dialogs_controller.rb
@@ -121,5 +121,17 @@ module Api
       raise BadRequestError, "Invalid template_class #{data['template_class']} for creating a service dialog from template" unless TEMPLATE_CLASSES.include?(data['template_class'])
       raise BadRequestError, "Invalid dialog_class #{data['dialog_class']} for creating a service dialog from template" unless DIALOG_CLASSES.include?(data['dialog_class'])
     end
+
+    def normalize_encrypted(value)
+      value
+    end
+
+    def api_resource_action_options
+      if @req.action == "refresh_dialog_fields" && @req.collection_id && @req.subcollection.blank?
+        super + %w[include_encrypted_attributes]
+      else
+        super
+      end
+    end
   end
 end
