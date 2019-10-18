407b45036ae66f99e7b7dd1deb48f78bd15eb08f
Daniel Berger <djberg96@gmail.com>
2019-10-18 13:12:04 -0400

    Add edit and delete actions to regions controller.

--- app/controllers/api/regions_controller.rb
+++ app/controllers/api/regions_controller.rb
@@ -1,4 +1,13 @@
 module Api
   class RegionsController < BaseController
+    INVALID_REGIONS_ATTRS = %w(id created_on updated_on).freeze
+
+    def edit_region(type, id, data)
+      bad_attrs = data_includes_invalid_attrs(data)
+      if bad_attrs.present?
+        raise BadRequestError, "Attributes #{bad_attrs} should not be specified for updating a region resource"
+      end
+      super
+    end
   end
 end
