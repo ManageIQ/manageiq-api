ad1d473c1d69fbe4e03efd16c18c608ee58f8344
Daniel Berger <djberg96@gmail.com>
2019-10-21 13:07:46 -0400

    Remove id from blacklist since it's already handled by the base controller, fix a couple rubocop warnings, add some comments.

--- app/controllers/api/regions_controller.rb
+++ app/controllers/api/regions_controller.rb
@@ -1,7 +1,11 @@
 module Api
   class RegionsController < BaseController
-    INVALID_REGIONS_ATTRS = %w(id created_at updated_at).freeze
+    INVALID_REGIONS_ATTRS = %w[created_at updated_at].freeze
 
+    # Edit an existing region (MiqRegion). Certain fields are meant for
+    # internal use only and may not be edited. Attempting to edit one of
+    # the forbidden fields will result in a bad request error.
+    #
     def edit_resource(type, id, data)
       bad_attrs = data_includes_invalid_attrs(data)
 
@@ -15,8 +19,13 @@ module Api
 
     private
 
+    # Check to see if any of the data attributes contain an invalid field.
+    # Returns a list of invalid fields as a comma separated string that you
+    # can use for error messages, or nil if the data argument is blank.
+    #
     def data_includes_invalid_attrs(data)
       return nil unless data
+
       data.keys.select { |key| INVALID_REGIONS_ATTRS.include?(key) }.compact.join(", ")
     end
   end
