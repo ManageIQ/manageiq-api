ef1a4fc3c6b8a638ace05c31ee9f3a56f159ae86
Daniel Berger <djberg96@gmail.com>
2019-10-21 10:16:04 -0400

    Add private method, fix time field names.

--- app/controllers/api/regions_controller.rb
+++ app/controllers/api/regions_controller.rb
@@ -1,13 +1,22 @@
 module Api
   class RegionsController < BaseController
-    INVALID_REGIONS_ATTRS = %w(id created_on updated_on).freeze
+    INVALID_REGIONS_ATTRS = %w(id created_at updated_at).freeze
 
     def edit_resource(type, id, data)
       bad_attrs = data_includes_invalid_attrs(data)
+
       if bad_attrs.present?
-        raise BadRequestError, "Attributes #{bad_attrs} should not be specified for updating a region resource"
+        msg = "Attributes #{bad_attrs} should not be specified for updating a region resource"
+        raise BadRequestError, msg
       end
+
       super
+    end
+
+    private
+
+    def data_includes_invalid_attrs(data)
+      data.keys.select { |k| INVALID_REGIONS_ATTRS.include?(k) }.compact.join(", ") if data
     end
   end
 end
