11d37dfea3daa184ba21f55b681d343c266c3c87
Daniel Berger <djberg96@gmail.com>
2019-10-21 09:59:57 -0400

    Update method name to edit_resource.

--- app/controllers/api/regions_controller.rb
+++ app/controllers/api/regions_controller.rb
@@ -2,7 +2,7 @@ module Api
   class RegionsController < BaseController
     INVALID_REGIONS_ATTRS = %w(id created_on updated_on).freeze
 
-    def edit_region(type, id, data)
+    def edit_resource(type, id, data)
       bad_attrs = data_includes_invalid_attrs(data)
       if bad_attrs.present?
         raise BadRequestError, "Attributes #{bad_attrs} should not be specified for updating a region resource"
