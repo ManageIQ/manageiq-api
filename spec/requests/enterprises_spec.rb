a04b9d55a629905e0fb5a4ee836e8e7843ed706c
Martin Povolny <mpovolny@redhat.com>
2019-10-24 19:53:16 +0200

    Add specs for newly added rbac identifiers.

--- spec/requests/enterprises_spec.rb
+++ spec/requests/enterprises_spec.rb
@@ -1,14 +1,25 @@
 describe "Enterprises API" do
   context "GET /api/enterprises" do
-    it "returns the enterprises" do
-      enterprise = MiqEnterprise.first
+    before do
+      @enterprise = MiqEnterprise.first
+    end
+
+    it "does not allow an unauthorized user to list the enterprises" do
       api_basic_authorize
+
+      get(api_enterprises_url)
+
+      expect(response).to have_http_status(:forbidden)
+    end
+
+    it "returns the enterprises" do
+      api_basic_authorize(:ems_infra)
 
       get(api_enterprises_url)
 
       expected = {
         "name"      => "enterprises",
-        "resources" => [{"href" => api_enterprise_url(nil, enterprise)}]
+        "resources" => [{"href" => api_enterprise_url(nil, @enterprise)}]
       }
       expect(response).to have_http_status(:ok)
       expect(response.parsed_body).to include(expected)
@@ -18,7 +29,7 @@ describe "Enterprises API" do
   context "GET /api/enterprises/:id" do
     it "returns the enterprise" do
       enterprise = FactoryBot.create(:miq_enterprise)
-      api_basic_authorize
+      api_basic_authorize(:ems_infra)
 
       get(api_enterprise_url(nil, enterprise))
 
