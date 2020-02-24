c330a9c6b8bf41ab1ef1278a1154d717d244e317
Alberto Bellotti <abellotti@users.noreply.github.com>
2020-02-24 13:03:15 -0500

    Merge pull request #749 from gtanzillo/enable-add-edit-policy-profiles
    Enable add and edit resource for /api/policy_profiles endpoint
    
    (cherry picked from commit 32ea08e8a31da847d5a3cf607733ed75b58bea6f)
    
    Fixes https://bugzilla.redhat.com/show_bug.cgi?id=1806702

--- app/controllers/api/policy_profiles_controller.rb
+++ app/controllers/api/policy_profiles_controller.rb
@@ -1,5 +1,17 @@
 module Api
   class PolicyProfilesController < BaseController
     include Subcollections::Policies
+
+    def edit_resource(type, id, _data = {})
+      raise ForbiddenError if collection_class(:policy_profiles).find(id).read_only?
+
+      super
+    end
+
+    def delete_resource(type, id, _data = {})
+      raise ForbiddenError if collection_class(:policy_profiles).find(id).read_only?
+
+      super
+    end
   end
 end
