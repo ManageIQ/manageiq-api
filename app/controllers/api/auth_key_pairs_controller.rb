8f53212f53243261f2717a0421cb42a217f2eed3
Milan Zazrivec <mzazrivec@redhat.com>
2020-04-01 14:13:23 +0200

    Add async key pair deletion

--- app/controllers/api/auth_key_pairs_controller.rb
+++ app/controllers/api/auth_key_pairs_controller.rb
@@ -1,4 +1,13 @@
 module Api
   class AuthKeyPairsController < BaseController
+    def delete_resource(type, id, _data = {})
+      delete_action_handler do
+        key_pair = resource_search(id, type, collection_class(type))
+        raise "Delete not supported for #{key_pair.name}" unless key_pair.respond_to?(:delete_key_pair_queue)
+
+        task_id = key_pair.delete_key_pair_queue(current_user.userid)
+        action_result(true, "Deleting #{key_pair.name}", :task_id => task_id)
+      end
+    end
   end
 end
