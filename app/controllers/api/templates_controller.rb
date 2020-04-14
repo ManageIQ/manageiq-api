f67885218e98873e0cc2f00318182df672023d50
Alberto Bellotti <abellotti@users.noreply.github.com>
2020-04-14 11:01:54 -0400

    Merge pull request #799 from lfu/check_compliance_templates
    Adding support for check_compliance action on templates
    
    (cherry picked from commit aa8c62da0e09f137281b822825ff3f8f2c35ffa9)

--- app/controllers/api/templates_controller.rb
+++ app/controllers/api/templates_controller.rb
@@ -7,5 +7,29 @@ module Api
     def edit_resource(type, id = nil, data = {})
       super(type, id, data.extract!('name', 'description'))
     end
+
+    def check_compliance_resource(type, id, _data = nil)
+      api_action(type, id) do |klass|
+        template = resource_search(id, type, klass)
+        api_log_info("Checking compliance of #{template_ident(template)}")
+        request_compliance_check(template)
+      end
+    end
+
+    private
+
+    def template_ident(template)
+      "Template id:#{template.id} name:'#{template.name}'"
+    end
+
+    def request_compliance_check(template)
+      desc = "#{template_ident(template)} check compliance requested"
+      raise "#{template_ident(template)} has no compliance policies assigned" unless template.has_compliance_policies?
+
+      task_id = queue_object_action(template, desc, :method_name => "check_compliance")
+      action_result(true, desc, :task_id => task_id)
+    rescue StandardError => err
+      action_result(false, err.to_s)
+    end
   end
 end
