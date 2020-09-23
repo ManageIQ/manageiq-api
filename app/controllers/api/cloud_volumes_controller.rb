090b8fb67a32ad7dc3b1fb7e5482ac1f54ed9afc
Dávid Halász <dhalasz@redhat.com>
2020-09-23 09:56:10 +0200

    Expose the safe_delete operation on cloud volumes

--- app/controllers/api/cloud_volumes_controller.rb
+++ app/controllers/api/cloud_volumes_controller.rb
@@ -2,6 +2,14 @@ module Api
   class CloudVolumesController < BaseController
     include Subcollections::Tags
 
+    def safe_delete_resource(type, id, _data = {})
+      delete_action_handler do
+        cloud_volume = resource_search(id, type, collection_class(:cloud_volumes))
+        task_id = cloud_volume.safe_delete_volume_queue(User.current_user)
+        action_result(true, "Safely Deleting Cloud Volume #{cloud_volume.name}", :task_id => task_id)
+      end
+    end
+
     def delete_resource(type, id, _data = {})
       delete_action_handler do
         cloud_volume = resource_search(id, type, collection_class(:cloud_volumes))
