087772e85a497b2bbd9d401273b5addde09e29ce
Adam Grare <agrare@redhat.com>
2021-04-09 10:03:40 -0400

    Merge pull request #1025 from NickLaMuro/endpoints_for_cloud_subnets
    Add endpoint for cloud subnets
    
    (cherry picked from commit f6d2ec38716fafe3f5017b9187ba8826cfbe661c)

--- app/controllers/api/cloud_subnets_controller.rb
+++ app/controllers/api/cloud_subnets_controller.rb
@@ -15,5 +15,42 @@ module Api
 
       render_options(:cloud_subnets, :form_schema => klass.params_for_create(ems))
     end
+
+    def create_resource(_type, _id = nil, data = {})
+      ems = ExtManagementSystem.find(data['ems_id'])
+      klass = CloudSubnet.class_by_ems(ems)
+      raise BadRequestError, "Cannot create cloud subnet for Provider #{ems.name}: #{klass.unsupported_reason(:create)}" unless klass.supports?(:create)
+
+      task_id = ems.create_cloud_subnet_queue(session[:userid], data.deep_symbolize_keys)
+      action_result(true, "Creating Cloud Subnet #{data['name']} for Provider: #{ems.name}", :task_id => task_id)
+    rescue => err
+      action_result(false, err.to_s)
+    end
+
+    def edit_resource(type, id, data)
+      cloud_subnet = resource_search(id, type, collection_class(:cloud_subnets))
+      raise BadRequestError, "Cannot update #{cloud_subnet_ident(cloud_subnet)}: #{cloud_subnet.unsupported_reason(:update)}" unless cloud_subnet.supports?(:update)
+
+      task_id = cloud_subnet.update_cloud_subnet_queue(session[:userid], data.deep_symbolize_keys)
+      action_result(true, "Updating #{cloud_subnet_ident(cloud_subnet)}", :task_id => task_id)
+    rescue => err
+      action_result(false, err.to_s)
+    end
+
+    def delete_resource(type, id, _data = {})
+      delete_action_handler do
+        cloud_subnet = resource_search(id, type, collection_class(:cloud_subnets))
+        raise BadRequestError, "Cannot delete #{cloud_subnet_ident(cloud_subnet)}: #{cloud_subnet.unsupported_reason(:delete)}" unless cloud_subnet.supports?(:delete)
+
+        task_id = cloud_subnet.delete_cloud_subnet_queue(session[:userid])
+        action_result(true, "Deleting #{cloud_subnet_ident(cloud_subnet)}", :task_id => task_id)
+      end
+    end
+
+    private
+
+    def cloud_subnet_ident(cloud_subnet)
+      "Cloud Subnet id: #{cloud_subnet.id} name: '#{cloud_subnet.name}'"
+    end
   end
 end
