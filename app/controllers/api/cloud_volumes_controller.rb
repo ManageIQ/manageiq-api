ca9decc4ef653bb318e789b56d988966e5e92f88
Kavya Nekkalapu <37085529+kavyanekkalapu@users.noreply.github.com>
2021-04-19 15:01:23 -0400

    Add supports_feature check for update

--- app/controllers/api/cloud_volumes_controller.rb
+++ app/controllers/api/cloud_volumes_controller.rb
@@ -19,6 +19,7 @@ module Api
     def edit_resource(type, id, data = {})
       raise BadRequestError, "Must specify an id for editing a #{type} resource" unless id
       cloud_volume = resource_search(id, type, collection_class(:cloud_volumes))
+      raise BadRequestError, cloud_volume.unsupported_reason(:update) unless cloud_volume.supports?(:update)
   
       task_id = cloud_volume.update_volume_queue(User.current_user, data)
       action_result(true, "Updating #{cloud_volume.name}", :task_id => task_id)
