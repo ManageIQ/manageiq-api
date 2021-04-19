272204ee6b3d4cfb71003605c29b2a978d96284e
Kavya Nekkalapu <knekkal@us.ibm.com>
2021-04-19 15:51:41 -0400

    Fix lint errors

--- app/controllers/api/cloud_volumes_controller.rb
+++ app/controllers/api/cloud_volumes_controller.rb
@@ -18,15 +18,17 @@ module Api
 
     def edit_resource(type, id, data = {})
       raise BadRequestError, "Must specify an id for editing a #{type} resource" unless id
+
       cloud_volume = resource_search(id, type, collection_class(:cloud_volumes))
+
       raise BadRequestError, cloud_volume.unsupported_reason(:update) unless cloud_volume.supports?(:update)
-  
+
       task_id = cloud_volume.update_volume_queue(User.current_user, data)
       action_result(true, "Updating #{cloud_volume.name}", :task_id => task_id)
     rescue => err
       action_result(false, err.to_s)
     end
-  
+
     def safe_delete_resource(type, id, _data = {})
       delete_action_handler do
         cloud_volume = resource_search(id, type, collection_class(:cloud_volumes))
