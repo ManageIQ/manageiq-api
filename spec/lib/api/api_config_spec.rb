ab1161f31ff97454dbee305bd7f201ebf478e068
Jason Frey <fryguy9@gmail.com>
2021-05-13 12:16:22 -0400

    Fix spec that verifies features to actually see all features

--- spec/lib/api/api_config_spec.rb
+++ spec/lib/api/api_config_spec.rb
@@ -22,60 +22,31 @@ describe 'API configuration (config/api.yml)' do
 
     describe 'identifiers' do
       let(:api_feature_identifiers) do
-        feature_identifiers { |set, id| set.add(id) }
-      end
+        collection_settings.each_with_object(Set.new) do |(_key, config), ids|
+          action_configs  = config.to_h.select { |k, v| k.to_s.end_with?("_actions") }.values
+          action_configs += Array(config.resource_entities).map { |e| Array(e.entity_actions) }
 
-      let(:sui_product_features) do
-        feature_identifiers { |set, id| set.add(id) if /^sui_/ =~ id }
+          action_configs.each do |action_config|
+            action_config.each do |_verb, actions|
+              actions.each do |action|
+                ids.merge(Array(action[:identifier]))
+                ids.merge(Array(action[:identifiers]).flat_map { |i| Array(i[:identifier]) })
+              end
+            end
+          end
+
+          ids.merge(Array(config[:identifier]))
+        end
       end
 
       it 'is not empty' do
         expect(api_feature_identifiers).not_to be_empty
       end
 
-      it 'contains only valid miq_feature identifiers' do
+      it 'contains only valid MiqProductFeature identifiers' do
         MiqProductFeature.seed_features
-        dangling = Array(api_feature_identifiers).reject { |feature| MiqProductFeature.feature_exists?(feature) }
-        expect(dangling).to be_empty
-      end
-
-      it 'contains valid sui specific miq_feature identifiers' do
-        expect(sui_product_features.subset?(api_feature_identifiers))
-      end
-
-      def feature_identifiers
-        collection_settings.each_with_object(Set.new) do |(_, cfg), set|
-          Array(cfg[:identifier]).each { |id| set.add(id) }
-          keys = %i(collection_actions resource_actions subcollection_actions subresource_actions)
-          Array(cfg[:subcollections]).each do |s|
-            keys << "#{s}_subcollection_actions" << "#{s}_subresource_actions"
-          end
-          keys.each do |action_type|
-            next unless cfg[action_type]
-            cfg[action_type].each do |_, method_cfg|
-              method_cfg.each do |action_cfg|
-                next unless action_cfg[:identifier]
-
-                Array(action_cfg[:identifier]).each { |id| yield(set, id) }
-              end
-            end
-          end
-          add_resource_entity_identifiers(cfg, set)
-        end
-      end
-
-      def add_resource_entity_identifiers(cfg, set)
-        return if cfg["resource_entities"].blank?
-
-        cfg["resource_entities"].each do |entity|
-          entity[:entity_actions].each do |_, method_cfg|
-            method_cfg.each do |action_cfg|
-              next unless action_cfg[:identifier]
-
-              Array(action_cfg[:identifier]).each { |id| set.add(id) }
-            end
-          end
-        end
+        invalid_features = api_feature_identifiers - MiqProductFeature.pluck(:identifier)
+        expect(invalid_features).to be_empty
       end
     end
   end
