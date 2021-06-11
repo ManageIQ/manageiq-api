a686ee90ec12a83c240bc50628ad392670d7982b
mengxiwuu <mengxiwuu@gmail.com>
2021-06-11 13:12:25 -0400

    expose snmp options in action controller

--- app/controllers/api/actions_controller.rb
+++ app/controllers/api/actions_controller.rb
@@ -11,7 +11,14 @@ module Api
     end
 
     def options
-      render_options(:actions, :action_types => MiqAction::TYPES)
+      render_options(:actions, build_action_options)
+    end
+
+    def build_action_options
+      {
+        :action_types => MiqAction::TYPES,
+        :snmp_trap    => MiqSnmp.available_types
+      }
     end
   end
 end
--- app/controllers/api/snmp_options_controller.rb
+++
@@ -1,7 +0,0 @@
-module Api
-    class SnmpOptionsController < BaseController
-      def options
-        render_options(:snmp_options, :snmp_options_types => MiqSnmp.available_types)
-      end
-    end
-end
