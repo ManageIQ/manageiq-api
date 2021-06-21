43142e8b40e088b0886c8081fde99e1fc387b0e6
Oleg Barenboim <chessbyte@gmail.com>
2021-06-21 16:41:20 -0400

    Simplify validate_*_for_action methods

--- app/controllers/api/vms_controller.rb
+++ app/controllers/api/vms_controller.rb
@@ -364,12 +364,7 @@ module Api
     end
 
     def validate_vm_for_action(vm, action)
-      if vm.respond_to?("supports_#{action}?")
-        action_result(vm.public_send("supports_#{action}?"), vm.unsupported_reason(action.to_sym))
-      else
-        validation = vm.send("validate_#{action}")
-        action_result(validation[:available], validation[:message].to_s)
-      end
+      action_result(vm.supports?(action), vm.unsupported_reason(action.to_sym))
     end
 
     def validate_vm_for_remote_console(vm, protocol = nil)
