de0bb1719eb2a09bbf18e484f6e87a080ef7a817
Gilbert Cherrie <Gilbert.Cherrie@ibm.com>
2021-09-15 13:00:53 -0400

    Add options call for network routers

--- app/controllers/api/network_routers_controller.rb
+++ app/controllers/api/network_routers_controller.rb
@@ -27,12 +27,39 @@ module Api
       delete_action_handler do
         network_router = resource_search(id, type, collection_class(:network_routers))
         raise "Delete not supported for #{network_router_ident(network_router)}" unless network_router.respond_to?(:delete_network_router_queue)
+
         task_id = network_router.delete_network_router_queue(User.current_user.id)
         action_result(true, "Deleting #{network_router_ident(network_router)}", :task_id => task_id)
       end
     end
 
+    def options
+      if params.key?("id")
+        options_by_id
+      elsif params.key?("ems_id")
+        options_by_ems_id
+      else
+        super
+      end
+    end
+
     private
+
+    def options_by_ems_id
+      ems = resource_search(params["ems_id"], :ext_management_systems, ExtManagementSystem)
+      klass = NetworkRouter.class_by_ems(ems)
+
+      raise BadRequestError, "No Cloud Network support for - #{ems.class}" unless defined?(ems.class::NetworkRouter)
+
+      raise BadRequestError, "No DDF specified for - #{klass}" unless klass.supports?(:create)
+
+      render_options(:cloud_networks, :form_schema => klass.params_for_create(ems, params["cn_id"]))
+    end
+
+    def options_by_id
+      network_router = resource_search(params["id"], :network_routers, NetworkRouter)
+      render_options(:network_routers, :form_schema => network_router.params_for_update)
+    end
 
     def network_router_ident(network_router)
       "Network Router id:#{network_router.id} name: '#{network_router.name}'"
