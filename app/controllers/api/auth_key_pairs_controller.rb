9d6e0b81ddc774568cc1a7d97e78b9504017229a
Oleg Barenboim <chessbyte@gmail.com>
2021-09-24 17:33:51 -0400

    Convert validate_delete_key_pair to use supports?(:delete)

--- app/controllers/api/auth_key_pairs_controller.rb
+++ app/controllers/api/auth_key_pairs_controller.rb
@@ -17,7 +17,7 @@ module Api
     def delete_resource(type, id, _data = {})
       delete_action_handler do
         key_pair = resource_search(id, type, collection_class(type))
-        raise "Delete not supported for #{key_pair.name}" unless key_pair.respond_to?(:delete_key_pair_queue)
+        raise "Delete not supported for #{key_pair.name}" unless key_pair.supports?(:delete)
 
         task_id = key_pair.delete_key_pair_queue(current_user.userid)
         action_result(true, "Deleting #{key_pair.name}", :task_id => task_id)
