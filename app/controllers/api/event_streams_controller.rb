83c6c4882d3159f92cdc44416ba3f0caf5b39c51
Joe Rafaniello <jrafanie@gmail.com>
2021-11-22 16:10:04 -0500

    Expose MiqEvent/EmsEvent group/levels in event stream OPTIONS
    The timeline_events key in the response body 'data' field contains information
    on the type of events, their descriptions, group levels, group names, etc.
    
    Currently, we only support EmsEvent and MiqEvent from event_streams.
    
    Part of #1090

--- app/controllers/api/event_streams_controller.rb
+++ app/controllers/api/event_streams_controller.rb
@@ -1,4 +1,13 @@
 module Api
   class EventStreamsController < BaseController
+    def options
+      render_options(@req.collection, build_additional_fields)
+    end
+
+    private
+
+    def build_additional_fields
+      {:timeline_events => EventStream.timeline_options}
+    end
   end
 end
