e610d0156135e1d0d4a2c3aa0662f70b7c056e34
GREGORYBEYER <Gregory.Beyer@kyndryl.com>
2021-11-23 15:42:07 +0200

    Creation of host initiator group

---
+++ app/controllers/api/host_initiator_groups_controller.rb
@@ -0,0 +1,17 @@
+module Api
+  class HostInitiatorGroupsController < BaseController
+    def create_resource(type, _id = nil, data = {})
+      raise BadRequestError, "ems_id not defined for #{type} resource" if data['ems_id'].blank?
+
+      ext_management_system = ExtManagementSystem.find(data['ems_id'])
+
+      klass = HostInitiatorGroup.class_by_ems(ext_management_system)
+      raise BadRequestError, klass.unsupported_reason(:create) unless klass.supports?(:create)
+
+      task_id = HostInitiatorGroup.create_host_initiator_group_queue(session[:userid], ext_management_system, data)
+      action_result(true, "Creating Host Initiator Group #{data['name']} for Provider: #{ext_management_system.name}", :task_id => task_id)
+    rescue => err
+      action_result(false, err.to_s)
+    end
+  end
+end
---
+++ spec/requests/host_initiator_groups_spec.rb
@@ -0,0 +1,74 @@
+describe "Host Initiator Groups API" do
+  include Spec::Support::SupportsHelper
+  context "POST /api/host_initiator_groups" do
+    it "with an invalid ems_id it responds with 404 Not Found" do
+      api_basic_authorize(collection_action_identifier(:host_initiator_groups, :create))
+
+      request = {
+        "action"   => "create",
+        "resource" => {
+          "ems_id"              => nil,
+          "name"                => "test_host_initiator_group",
+          "physical_storage_id" => "1",
+        }
+      }
+
+      post(api_host_initiator_groups_url, :params => request)
+
+      expect(response).to have_http_status(:bad_request)
+    end
+
+    it "creates new Host Initiator Group" do
+      api_basic_authorize(collection_action_identifier(:host_initiator_groups, :create))
+      provider = FactoryBot.create(:ems_autosde, :name => 'Autosde')
+      request = {
+        "action"   => "create",
+        "resource" => {
+          "ems_id"              => provider.id,
+          "name"                => "test_host_initiator_group",
+          "physical_storage_id" => provider.id.to_s,
+        }
+      }
+
+      post(api_host_initiator_groups_url, :params => request)
+
+      expect_multiple_action_result(1, :success => true, :message => "Creating Host Initiator Group test_host_initiator_group for Provider: #{provider.name}", :task => true)
+    end
+
+    it "Refuses to create without appropriate role" do
+      api_basic_authorize
+      provider = FactoryBot.create(:ems_autosde, :name => 'Autosde')
+      request = {
+        "action"   => "create",
+        "resource" => {
+          "ems_id"              => provider.id,
+          "name"                => "test_host_initiator_group",
+          "physical_storage_id" => provider.id.to_s,
+        }
+      }
+
+      post(api_host_initiator_groups_url, :params => request)
+      expect(response).to have_http_status(:forbidden)
+    end
+
+    it "Won't create for unsupported models" do
+      api_basic_authorize(collection_action_identifier(:host_initiator_groups, :create))
+      provider = FactoryBot.create(:ems_autosde, :name => 'Autosde')
+
+      stub_supports_not(ManageIQ::Providers::Autosde::StorageManager::HostInitiatorGroup, :create)
+
+      request = {
+        "action"   => "create",
+        "resource" => {
+          "ems_id"              => provider.id,
+          "name"                => "test_host_initiator_group",
+          "physical_storage_id" => provider.id.to_s,
+        }
+      }
+
+      post(api_host_initiator_groups_url, :params => request)
+
+      expect(response).to have_http_status(:bad_request)
+    end
+  end
+end
