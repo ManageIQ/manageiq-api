cc96cefd9f2da09c875466d26856bddd73717ec3
Mengxi Wu <mengxiwu@Mengxis-MBP.attlocal.net>
2021-12-10 12:04:35 -0500

    added option call for floating ips

--- app/controllers/api/floating_ips_controller.rb
+++ app/controllers/api/floating_ips_controller.rb
@@ -26,6 +26,16 @@ module Api
       {:task_id => floating_ip.delete_floating_ip_queue(User.current_userid)}
     end
 
+    def options
+      if (id = params["id"])
+        render_update_resource_options(id)
+      elsif (ems_id = params["ems_id"])
+        render_create_resource_options(ems_id)
+      else
+        super
+      end
+    end
+
     private
 
     def floating_ip_ident(floating_ip)
