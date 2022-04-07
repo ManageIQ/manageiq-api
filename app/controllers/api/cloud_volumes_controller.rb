aaa61cda404f8a241613d554b644ef2723c8c88a
Melody-Ann-Seda-Marotte <Melody.Ann.Seda.Marotte@ibm.com>
2022-04-07 10:06:55 -0700

    testing

--- app/controllers/api/cloud_volumes_controller.rb
+++ app/controllers/api/cloud_volumes_controller.rb
@@ -34,13 +34,47 @@ module Api
 
     def options
       if (id = params["id"])
+        p "========================= mels id was called"
         render_update_resource_options(id)
       elsif (ems_id = params["ems_id"])
+        p "========================= mels ems_id"
         render_create_resource_options(ems_id)
+        ## we can have a check for another param to see how it is maybe? if ems_id then if is also attach
+      elsif (ems_id_attach = params["ems_id_attach"])
+        p "========================= mels ems_id_attach"
+        render_attach_resource_options_mels(ems_id_attach) ## this figures out the provider on its own
       else
         super
       end
     end
+
+    def render_attach_resource_options_mels(ems_id)
+      p "-------------------------- mels attach options "
+      type = @req.collection.to_sym
+      base_klass = collection_class(type)
+
+      ems = resource_search(ems_id, :providers)
+      klass = ems.class_by_ems(base_klass.name)
+      raise BadRequestError, "No #{type.to_s.titleize} support for - #{ems.name}" unless klass
+      raise BadRequestError, klass.unsupported_reason(:create) unless klass.supports?(:create)
+
+      p "content"
+      p klass
+      p type
+      p ems
+      p "---------------------- mels render options"
+      render_options(type, :form_schema => params_for_attach(ems)) ## resource or class
+    end
+
+    # def render_update_resource_options_mels(id)
+    #   p "-------------------------- mels update attach options "
+    #   type = @req.collection.to_sym
+    #   resource = resource_search(id, type)
+    #   raise BadRequestError, resource.unsupported_reason(:update) unless resource.supports?(:update)
+
+    #   p "---------------------- mels render options"
+    #   render_options(type, :form_schema => resource.params_for_attach)
+    # end
 
     def create_backup_resource(type, id, data)
       api_resource(type, id, "Creating backup", :supports => :backup_create) do |cloud_volume|
