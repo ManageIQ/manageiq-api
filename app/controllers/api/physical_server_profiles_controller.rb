73475562a76a0a4c7d4b4451698cb56ba4a9b664
Anže Žitnik <anze.zitnik@xlab.si>
2022-04-14 17:28:18 +0200

    Add POST endpoints: assign, deploy, unassign.

--- app/controllers/api/physical_server_profiles_controller.rb
+++ app/controllers/api/physical_server_profiles_controller.rb
@@ -2,7 +2,19 @@ module Api
   class PhysicalServerProfilesController < BaseController
     include Subcollections::EventStreams
 
+    def assign_server_resource(type, id, data)
+      enqueue_ems_action(type, id, :method_name => :assign_server, :args => [data["server_id"]]) do
+        ensure_resource_exists(:physical_servers, data["server_id"])
+      end
+    end
 
+    def deploy_server_resource(type, id, _data)
+      enqueue_ems_action(type, id, :method_name => :deploy_server)
+    end
+
+    def unassign_server_resource(type, id, _data)
+      enqueue_ems_action(type, id, :method_name => :unassign_server)
+    end
 
     private
 
