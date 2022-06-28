6c29a0418221d2b739a9683b9f2135306a772c37
tzurvaza <tzurvaza@gmail.com>
2022-06-28 20:17:43 +0300

    remove empty line

--- app/controllers/api/volume_mappings_controller.rb
+++ app/controllers/api/volume_mappings_controller.rb
@@ -1,6 +1,5 @@
 module Api
   class VolumeMappingsController < BaseController
-
     def refresh_resource(type, id, _data = nil)
       enqueue_ems_action(type, id, "Refreshing", :method_name => :refresh_ems)
     end
