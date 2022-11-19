fb9b172ed81c05723146a66bd2a8b43ad6a9d973
Gal Oiring <74841666+galoiring@users.noreply.github.com>
2022-11-19 23:52:48 +0200

    Added endpoints and their respective specs for physical storage families, storage resources and storage services

---
+++ app/controllers/api/storage_resources_controller.rb
@@ -0,0 +1,4 @@
+module Api
+  class StorageResourcesController < BaseController
+  end
+end
---
+++ spec/requests/storage_services_spec.rb
@@ -0,0 +1,33 @@
+describe "Storage Services API" do
+  fcontext "GET /api/storage_services" do
+    it "returns all storage_services" do
+      storage_service = FactoryBot.create(:storage_service)
+      api_basic_authorize('storage_service_show_list')
+
+      get(api_storage_services_url)
+
+      expected = {
+        "name"      => "storage_services",
+        "resources" => [{"href" => api_storage_service_url(nil, storage_service)}]
+      }
+      expect(response).to have_http_status(:ok)
+      expect(response.parsed_body).to include(expected)
+    end
+  end
+
+  context "GET /api/storage_services/:id" do
+    it "returns one storage_service" do
+      storage_service = FactoryBot.create(:storage_services)
+      api_basic_authorize('storage_service_show')
+
+      get(api_storage_service_url(nil, storage_service))
+
+      expected = {
+        "name" => storage_service.name,
+        "href" => api_storage_service_url(nil, storage_service)
+      }
+      expect(response).to have_http_status(:ok)
+      expect(response.parsed_body).to include(expected)
+    end
+  end
+end
