e5ce5f90a0bc2b52f21bbf7b84a0ef0aec425aa2
Keenan Brock <keenan@thebrocks.net>
2023-03-08 11:16:27 -0500

    Update hosts#edit to handle newer options format
    it is easier in DDF to just support the same authentications format as
    we want for update_authentications
    
    Keeping the old edit around to support the legacy controller until
    the ui is react-ified
    
    This will be the same format as used in validate_authenticaitons

--- app/controllers/api/hosts_controller.rb
+++ app/controllers/api/hosts_controller.rb
@@ -1,6 +1,7 @@
 module Api
   class HostsController < BaseController
     CREDENTIALS_ATTR = "credentials".freeze
+    AUTH_ATTR = "authentications".freeze
     AUTH_TYPE_ATTR = "auth_type".freeze
     DEFAULT_AUTH_TYPE = "default".freeze
 
@@ -11,21 +12,37 @@ module Api
     include Subcollections::Tags
 
     def edit_resource(type, id, data = {})
+      # TODO: drop 'credentials' parameter field when ui-classic hosts is in react
       credentials = data.delete(CREDENTIALS_ATTR)
+      authentications = data.delete(AUTH_ATTR)
       raise BadRequestError, "Cannot update non-credentials attributes of host resource" if data.any?
       resource_search(id, type).tap do |host|
+        # begin legacy ui-classic
         all_credentials = Array.wrap(credentials).each_with_object({}) do |creds, hash|
           auth_type = creds.delete(AUTH_TYPE_ATTR) || DEFAULT_AUTH_TYPE
           creds.symbolize_keys!
           creds.reverse_merge!(:userid => host.authentication_userid(auth_type))
           hash[auth_type.to_sym] = creds
         end
+        # end legacy ui-classic. if they provided the newer authentications, it will overwrite
+        all_credentials, _ = symbolize_password_keys!(authentications) if authentications
         host.update_authentication(all_credentials) if all_credentials.present?
       end
     end
 
     def check_compliance_resource(type, id, _data = nil)
       enqueue_ems_action(type, id, "Check Compliance for", :method_name => "check_compliance", :supports => true)
+    end
+
+    private
+
+    # takes credentials from params and converts into something for update_authentications
+    def symbolize_password_keys!(authentications)
+      auth_type = authentications.keys.first
+      # symbolize userid, password
+      authentications[auth_type].symbolize_keys!
+
+      return authentications, auth_type
     end
   end
 end
