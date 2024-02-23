fe6fd8ef48dc636d7df4fd07a9d8f3d3b3586458
Keenan Brock <keenan@thebrocks.net>
2024-02-23 03:17:59 -0500

    Add MiqTask#deliver and execute

--- app/controllers/api/request_tasks_controller.rb
+++ app/controllers/api/request_tasks_controller.rb
@@ -1,5 +1,15 @@
 module Api
   class RequestTasksController < BaseController
     include Api::Mixins::ResourceCancel
+
+    # execute queues the work.
+    # this is typically called from within the workflow
+    def execute_resource(type, id, _data)
+      api_resource(type, id, "Executing") do |task|
+        raise BadRequestError, "Resource must be approved. state is #{task.state}" unless task.approved?
+
+        task.execute_queue
+      end
+    end
   end
 end
