270cebeb16a3844333b73a49b149d925b0a5b814
chitra607 <chitra.r@xaasio.com>
2025-07-17 10:54:02 +0000

    updated

--- app/controllers/api/container_volumes_controller.rb
+++ app/controllers/api/container_volumes_controller.rb
@@ -1,6 +1,6 @@
 module Api
   class ContainerVolumesController < BaseController
-    def attach_resource(type, id, data = {})
+    def attach_resource(data = {})
       vm_id = data["vm_id"] || params[:c_id]
       raise BadRequestError, "Must specify a vm_id" if vm_id.blank?
 
@@ -13,16 +13,16 @@ module Api
         raise BadRequestError, "VM does not support attach"
       end
 
-      {:task_id => vm.raw_attach_volume( vm, pvc_name, volume_name)}
+      {:task_id => vm.raw_attach_volume(vm, pvc_name, volume_name)}
     rescue => err
       action_result(false, err.to_s)
     end
 
-    def create_and_attach_volume_resource(type, id, data = {})
+    def create_and_attach_volume_resource(data = {})
       vm_id = data["vm_id"] || params[:c_id]
       raise BadRequestError, "Must specify a vm_id" if vm_id.blank?
 
-      volume_name    = data["volume_name"] || params.dig(:resource, :volume_name)
+      volume_name = data["volume_name"] || params.dig(:resource, :volume_name)
       volume_size = data["volume_size"] || params.dig(:resource, :volume_size)
 
       vm = resource_search(vm_id, :vms)
@@ -31,12 +31,12 @@ module Api
         raise BadRequestError, "VM does not support attach"
       end
 
-      {:task_id => vm.create_pvc( vm, volume_name, volume_size)}
+      {:task_id => vm.create_pvc(vm, volume_name, volume_size)}
     rescue => err
       action_result(false, err.to_s)
     end
 
-    def detach_resource(type, id, data = {})
+    def detach_resource(data = {})
       vm_id = data["vm_id"] || params[:c_id]
       raise BadRequestError, "Must specify a vm_id" if data["vm_id"].blank?
 
@@ -47,6 +47,5 @@ module Api
     rescue => err
       action_result(false, err.to_s)
     end
-    
   end
 end
